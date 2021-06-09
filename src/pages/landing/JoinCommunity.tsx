a139c09891b3e4cbe99de3dba40948f893b6ad26
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:16:59 +1200

    :sparkles: create join community component

---
+++ src/pages/landing/JoinCommunity.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledJoinCommunity = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const JoinCommunity = () => {
+  return <StyledJoinCommunity>join community</StyledJoinCommunity>;
+};
+
+export default JoinCommunity;
