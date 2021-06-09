a3603c9ac6155bfe62a7bba17487346dc3beea6e
Chase Manning <me@chasemanning.co.nz>
2021-06-09 18:28:12 +1200

    :rocket: add nav items to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../assets/logo.svg";
+import NavItems from "./NavItems";
 
 const StyledHeader = styled.div`
   width: 100%;
@@ -13,10 +14,17 @@ const Logo = styled.img`
   height: 4.8rem;
 `;
 
+const Actions = styled.div`
+  display: flex;
+`;
+
 const Header = () => {
   return (
     <StyledHeader>
       <Logo src={logo} />
+      <Actions>
+        <NavItems />
+      </Actions>
     </StyledHeader>
   );
 };
