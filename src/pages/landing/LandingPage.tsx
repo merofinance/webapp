0eaecd79d5018fcf1138c1a1b2090dd36503680d
Chase Manning <me@chasemanning.co.nz>
2021-06-09 17:42:19 +1200

    :sparkles: create landing page component

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLanding = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LandingPage = () => {
+  return <StyledLanding></StyledLanding>;
+};
+
+export default LandingPage;
