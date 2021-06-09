f0bab285e50a681e51097d0ddaf22cab2d65d15d
Chase Manning <me@chasemanning.co.nz>
2021-06-09 20:06:22 +1200

    :lipstick: add hover effect for button

--- src/styles/Button.tsx
+++ src/styles/Button.tsx
@@ -8,14 +8,27 @@ const Button = styled.button`
   align-items: center;
   padding: 0 5rem;
   background-image: linear-gradient(
-    265.33deg,
+    270deg,
     var(--secondary-gradient) 0%,
-    var(--primary-gradient) 100%
+    var(--primary-gradient) 50%,
+    var(--secondary-gradient) 100%
   );
   font-size: 2.1rem;
   font-weight: 700;
   text-transform: capitalize;
   margin-top: 6rem;
+  cursor: pointer;
+  transition: 0.5s;
+  background-size: 200% auto;
+
+  :hover {
+    background-position: right center;
+    transform: scale(1.01);
+  }
+
+  :active {
+    transform: scale(0.99);
+  }
 `;
 
 export default Button;
