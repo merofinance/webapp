3827a008b67025e6b6b938d747d6af0ed1d6d880
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:16:43 +1200

    :sparkles: create benefits component

---
+++ src/pages/landing/Benefits.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledBenefits = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const Benefits = () => {
+  return <StyledBenefits>benefits</StyledBenefits>;
+};
+
+export default Benefits;
