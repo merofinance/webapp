444c60e18030f0afd673ffe14c71029a2ff0dd3c
Chase Manning <me@chasemanning.co.nz>
2021-06-09 19:57:27 +1200

    :sparkles: create hero section component

---
+++ src/pages/landing/Hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../../styles/Button";
+import { Header2, Header4 } from "../../styles/Headers";
+
+const StyledHero = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: var(--section-margin);
+`;
+
+const Hero = () => {
+  return (
+    <StyledHero>
+      <Header2>reactive liquidity</Header2>
+      <Header4>
+        A trustless and interest generating protocol designed to prevent collateralized loans from
+        becoming liquidable.
+      </Header4>
+      <Button>open app</Button>
+    </StyledHero>
+  );
+};
+
+export default Hero;
