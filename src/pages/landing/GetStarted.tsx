9514a7186870d592534e4c4befe5a1099178b473
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:16:48 +1200

    :sparkles: create get started component

---
+++ src/pages/landing/GetStarted.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledGetStarted = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const GetStarted = () => {
+  return <StyledGetStarted>get started</StyledGetStarted>;
+};
+
+export default GetStarted;
