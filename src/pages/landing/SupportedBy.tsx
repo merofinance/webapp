1a6ec12204c65baff30ed7bc2fc87485ea4a28f0
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:17:11 +1200

    :sparkles: create supported by component

---
+++ src/pages/landing/SupportedBy.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSupportedBy = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const SupportedBy = () => {
+  return <StyledSupportedBy>supported by</StyledSupportedBy>;
+};
+
+export default SupportedBy;
