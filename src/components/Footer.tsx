1504362d029f7f382de2f4076f7363cb1b724234
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:16:32 +1200

    :sparkles: create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const Footer = () => {
+  return <StyledFooter>footer</StyledFooter>;
+};
+
+export default Footer;
