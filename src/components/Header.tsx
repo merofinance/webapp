f754af8e5220682865bc8887446ace8dbc9c01dd
Chase Manning <me@chasemanning.co.nz>
2021-06-09 18:07:43 +1200

    :sparkles: create new header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Header = () => {
+  return <StyledHeader>meow</StyledHeader>;
+};
+
+export default Header;
