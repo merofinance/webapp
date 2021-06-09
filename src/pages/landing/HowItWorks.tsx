35ae286b12745e7518eee4567efd40746f5541dd
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:16:53 +1200

    :sparkles: create how it works component

---
+++ src/pages/landing/HowItWorks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHowItWorks = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const HowItWorks = () => {
+  return <StyledHowItWorks>how it works</StyledHowItWorks>;
+};
+
+export default HowItWorks;
