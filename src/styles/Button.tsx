c6cb3b6675fd01c5c09a12343af5081e8f4a0bea
Chase Manning <me@chasemanning.co.nz>
2021-06-09 19:57:15 +1200

    :sparkles: create button styled component

---
+++ src/styles/Button.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 6.2rem;
+  border-radius: 3.1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5rem;
+  background-image: linear-gradient(
+    265.33deg,
+    var(--secondary-gradient) 0%,
+    var(--primary-gradient) 100%
+  );
+  font-size: 2.1rem;
+  font-weight: 700;
+  text-transform: capitalize;
+`;
+
+export default Button;
