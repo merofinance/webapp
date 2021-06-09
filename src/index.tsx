7974bf3fd59b5f597e1351aaf979b35f70a6b614
Chase Manning <me@chasemanning.co.nz>
2021-06-09 19:19:56 +1200

    :art: change index to use global styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { persistor, store } from "./app/store";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyles />
         <App />
       </PersistGate>
     </Provider>
