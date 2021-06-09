425d01a3aaef2ab8accd7accb8c9e9b7bc8abfa4
Chase Manning <me@chasemanning.co.nz>
2021-06-09 19:57:07 +1200

    :sparkles: create header styles

---
+++ src/styles/Headers.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Header2 = styled.h2`
+  text-transform: capitalize;
+  font-weight: 700;
+  font-size: 7.2rem;
+  line-height: 8.64rem;
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+export const Header4 = styled.h4`
+  text-transform: capitalize;
+  font-weight: 400;
+  font-size: 2.8rem;
+  line-height: 4.2rem;
+  text-align: center;
+  max-width: 84rem;
+`;
