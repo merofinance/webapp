499f3d32de55ca1326970906c29eb31f78c0ff0f
Chase Manning <me@chasemanning.co.nz>
2021-06-09 21:17:05 +1200

    :sparkles: create preview component

---
+++ src/pages/landing/Preview.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPreview = styled.div`
+  width: 100%;
+  margin: var(--section-margin);
+`;
+
+const Preview = () => {
+  return <StyledPreview>preview</StyledPreview>;
+};
+
+export default Preview;
