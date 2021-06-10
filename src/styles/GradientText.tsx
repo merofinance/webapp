49994a8ccbecd52f8dd3d75371952e5520015ea5
Chase Manning <me@chasemanning.co.nz>
2021-06-10 18:54:34 +1200

    :sparkles: create gradient text style

---
+++ src/styles/GradientText.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const GradientText = styled.p`
+  background: var(--gradient);
+  background-clip: text;
+  text-fill-color: transparent;
+`;
+
+export default GradientText;
