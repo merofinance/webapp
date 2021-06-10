b06a41184ac7b9934f50c0d4fa6473ac437e6681
Chase Manning <me@chasemanning.co.nz>
2021-06-10 19:15:43 +1200

    :bug: gradient text not working

--- src/styles/GradientText.tsx
+++ src/styles/GradientText.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 const GradientText = styled.p`
   background: var(--gradient);
   background-clip: text;
-  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 export default GradientText;
