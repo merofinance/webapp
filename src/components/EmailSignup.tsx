3afcad2775f744725cd36598ea8fc17e33814970
Chase Manning <me@chasemanning.co.nz>
2021-06-12 13:18:36 +1200

    :sparkles: create email signup component

---
+++ src/components/EmailSignup.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import styled from "styled-components";
+import Button from "../styles/Button";
+
+const StyledEmailSignup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  font-weight: 700;
+  font-size: 1.6rem;
+  margin-bottom: 1.2rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+`;
+
+type InputProps = {
+  valid: boolean;
+};
+
+const Input = styled.input`
+  border: ${(props: InputProps) =>
+    props.valid ? "solid 1px var(--main)" : "solid 1px var(--error)"};
+  width: 26rem;
+  height: 56rem;
+  background-color: none;
+  padding: 0 1.2rem;
+  font-size: 1.6rem;
+  font-weight: 400;
+`;
+
+const SubmitButton = styled(Button)`
+  width: 11.3rem;
+  height: 5.6rem;
+  border-radius: 14px;
+  font-weight: 700;
+  font-size: 1.4rem;
+  margin-left: 1.6rem;
+`;
+
+const Note = styled.div`
+  font-weight: 400;
+  font-size: 1.2rem;
+`;
+
+const EmailSignup = () => {
+  const [email, setEmail] = useState("");
+  const [valid, setValid] = useState(true);
+  const [validate, setValidate] = useState(false);
+
+  const validateEmail = (email: string): boolean => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const _valid = re.test(String(email).toLowerCase());
+    setValid(_valid);
+    return _valid;
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (validate) setValid(validateEmail(e.target.value));
+    setEmail(e.target.value);
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setValidate(true);
+    const _valid = validateEmail(email);
+    if (_valid) alert("Email Signup Not Implemented");
+  };
+
+  return (
+    <StyledEmailSignup>
+      <Header>Receive latest Backd updates</Header>
+      <Form onSubmit={onSubmit}>
+        <Input valid={valid} type="email" value={email} onChange={onChange} />
+        <SubmitButton type="submit">submit</SubmitButton>
+      </Form>
+      <Note>We don’t share this with anyone.</Note>
+    </StyledEmailSignup>
+  );
+};
+
+export default EmailSignup;
