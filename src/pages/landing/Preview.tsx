99b33ab83b1eb63d0dac8d868fe27272799e7e83
Chase Manning <me@chasemanning.co.nz>
2021-06-13 18:28:26 +1200

    :iphone: preview sectioin

--- src/pages/landing/Preview.tsx
+++ src/pages/landing/Preview.tsx
@@ -4,6 +4,10 @@ import styled from "styled-components";
 const StyledPreview = styled.div`
   width: 100%;
   margin: var(--section-margin);
+
+  @media (max-width: 600px) {
+    margin: var(--mobile-section-margin);
+  }
 `;
 
 const Preview = () => {
