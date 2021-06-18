b7319ab9155d5041e84381c26e230e0300bf7dee
Chase Manning <me@chasemanning.co.nz>
2021-06-19 08:32:28 +1200

    :sparkles: add loading mode to button

--- src/components/styles/Button.tsx
+++ src/components/styles/Button.tsx
@@ -1,3 +1,4 @@
+import { CircularProgress } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
 
@@ -5,9 +6,11 @@ type ButtonProps = {
   hero?: boolean;
   large?: boolean;
   square?: boolean;
+  loading?: boolean;
 };
 
 const StyledButton = styled.button`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,7 +20,6 @@ const StyledButton = styled.button`
     var(--secondary-gradient) 50%,
     var(--primary-gradient) 100%
   );
-  text-transform: capitalize;
   cursor: pointer;
   transition: 0.5s;
   background-size: 200% auto;
@@ -26,24 +28,6 @@ const StyledButton = styled.button`
     background-position: right center;
   }
 
-  letter-spacing: ${(props: ButtonProps) => {
-    if (props.large) return "0";
-    return "0.46px";
-  }};
-  line-height: ${(props: ButtonProps) => {
-    if (props.large) return "2.8rem";
-    return "2.6rem";
-  }};
-  font-size: ${(props: ButtonProps) => {
-    if (props.large) return "2.1rem";
-    if (props.square) return "1.4rem";
-    return "1.5rem";
-  }};
-  font-weight: ${(props: ButtonProps) => {
-    if (props.large) return "700";
-    if (props.square) return "700";
-    return "500";
-  }};
   height: ${(props: ButtonProps) => {
     if (props.large) return "6.2rem";
     if (props.square) return "5.6rem";
@@ -66,8 +50,6 @@ const StyledButton = styled.button`
   }};
 
   @media (max-width: 600px) {
-    font-size: 1.4rem;
-
     margin-top: ${(props: ButtonProps) => {
       if (props.hero) return "4.5rem";
       return "0";
@@ -88,23 +70,68 @@ const StyledButton = styled.button`
   }
 `;
 
+const Text = styled.div`
+  text-transform: capitalize;
+  opacity: ${(props: ButtonProps) => (props.loading ? "0" : "1")};
+
+  letter-spacing: ${(props: ButtonProps) => {
+    if (props.large) return "0";
+    return "0.46px";
+  }};
+  line-height: ${(props: ButtonProps) => {
+    if (props.large) return "2.8rem";
+    return "2.6rem";
+  }};
+  font-size: ${(props: ButtonProps) => {
+    if (props.large) return "2.1rem";
+    if (props.square) return "1.4rem";
+    return "1.5rem";
+  }};
+  font-weight: ${(props: ButtonProps) => {
+    if (props.large) return "700";
+    if (props.square) return "700";
+    return "500";
+  }};
+
+  @media (max-width: 600px) {
+    font-size: 1.4rem;
+  }
+`;
+
+const ProgressContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
 type Props = {
   text: string;
   click: () => void;
   hero?: boolean;
   large?: boolean;
   square?: boolean;
+  loading?: boolean;
 };
 
 const Button = (props: Props) => {
   return (
     <StyledButton
-      onClick={() => props.click()}
+      onClick={() => {
+        if (!props.loading) props.click();
+      }}
       hero={props.hero}
       large={props.large}
       square={props.square}
     >
-      {props.text}
+      {props.loading && (
+        <ProgressContainer>
+          <CircularProgress size={props.large ? 31 : 25} />
+        </ProgressContainer>
+      )}
+      <Text hero={props.hero} large={props.large} square={props.square} loading={props.loading}>
+        {props.text}
+      </Text>
     </StyledButton>
   );
 };
