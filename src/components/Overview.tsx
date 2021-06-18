75c80d30918e790adf75406abca275d8a470fe3e
Chase Manning <me@chasemanning.co.nz>
2021-06-19 10:44:19 +1200

    :lipstick: minor formatting tweaks

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -7,7 +7,7 @@ const StyledOverview = styled.div`
   width: 40rem;
   display: flex;
   flex-direction: column;
-  padding: 1.2rem 1.8rem;
+  padding: 2rem 1.8rem;
   background-color: rgba(37, 33, 64, 0.4);
   border-radius: 1.4rem;
   box-shadow: 0px 0px 12px rgba(23, 18, 22, 0.05);
@@ -17,21 +17,20 @@ const StyledOverview = styled.div`
 const Header = styled.div`
   font-weight: 700;
   font-size: 2.4rem;
-  line-height: 4.2rem;
   letter-spacing: 0.25px;
-  margin-bottom: 1.6rem;
+  margin-bottom: 0.6rem;
 `;
 
 const StatisticContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  margin-top: 1.4rem;
 `;
 
 const LabelContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 1.4rem;
 `;
 
 const Label = styled.div`
@@ -56,18 +55,20 @@ type Props = {
 
 const Overview = (props: Props) => {
   return (
-    <StyledOverview>
-      <Header>{props.header}</Header>
-      {props.statistics.map((statistic: StatisticType) => (
-        <StatisticContainer>
-          <LabelContainer>
-            <Label>{statistic.header}</Label>
-            <Tooltip content={statistic.tooltip} />
-          </LabelContainer>
-          <Value>{statistic.value}</Value>
-        </StatisticContainer>
-      ))}
-    </StyledOverview>
+    <div>
+      <StyledOverview>
+        <Header>{props.header}</Header>
+        {props.statistics.map((statistic: StatisticType) => (
+          <StatisticContainer>
+            <LabelContainer>
+              <Label>{statistic.header}</Label>
+              <Tooltip content={statistic.tooltip} />
+            </LabelContainer>
+            <Value>{statistic.value}</Value>
+          </StatisticContainer>
+        ))}
+      </StyledOverview>
+    </div>
   );
 };
 
