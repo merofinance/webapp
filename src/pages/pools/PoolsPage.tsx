58ead3588d90e225c7ec025f6dbeac954cd432c2
Chase Manning <me@chasemanning.co.nz>
2021-06-19 10:23:08 +1200

    :rocket: add content to pools page

--- src/pages/pools/PoolsPage.tsx
+++ src/pages/pools/PoolsPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import ContentSection from "../../components/ContentSection";
+import PoolsOverview from "./PoolsOverview";
 
 const StyledPoolsPage = styled.div`
   width: 100%;
@@ -8,7 +10,35 @@ const StyledPoolsPage = styled.div`
 `;
 
 const PoolsPage = () => {
-  return <StyledPoolsPage></StyledPoolsPage>;
+  return (
+    <StyledPoolsPage>
+      <ContentSection
+        header="All pools"
+        statistics={[
+          {
+            header: "Your deposits",
+            value: "$0.00",
+            tooltip:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+          },
+          {
+            header: "Locked in position",
+            value: "$0.00",
+            tooltip:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+          },
+          {
+            header: "Rewards accrued",
+            value: "$0.00",
+            tooltip:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+          },
+        ]}
+        content={<p>test</p>}
+      />
+      <PoolsOverview />
+    </StyledPoolsPage>
+  );
 };
 
 export default PoolsPage;
