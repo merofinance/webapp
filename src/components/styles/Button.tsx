fbcddaf1d92e1d959ebadd8b0193e9a5d12d410b
Chase Manning <me@chasemanning.co.nz>
2021-06-19 08:04:34 +1200

    :art: change button conditionals to if else

--- src/components/styles/Button.tsx
+++ src/components/styles/Button.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 type ButtonProps = {
   hero?: boolean;
   large?: boolean;
+  square?: boolean;
 };
 
 const StyledButton = styled.button`
@@ -21,25 +22,54 @@ const StyledButton = styled.button`
   transition: 0.5s;
   background-size: 200% auto;
 
-  letter-spacing: ${(props: ButtonProps) => (props.large ? "0" : "0.46px")};
-  line-height: ${(props: ButtonProps) => (props.large ? "2.8rem" : "2.6rem")};
-  font-size: ${(props: ButtonProps) => (props.large ? "2.1rem" : "1.5rem")};
-  font-weight: ${(props: ButtonProps) => (props.large ? "700" : "500")};
-  height: ${(props: ButtonProps) => (props.large ? "6.2rem" : "3.8rem")};
-  border-radius: ${(props: ButtonProps) => (props.large ? "3.1rem" : "1.9rem")};
-  padding: 0 ${(props: ButtonProps) => (props.large ? "5rem" : "2.6rem")};
-  margin-top: ${(props: ButtonProps) => (props.hero ? "8rem" : "0")};
-
   :hover {
     background-position: right center;
   }
+
+  letter-spacing: ${(props: ButtonProps) => {
+    if (props.large) return "0";
+    return "0.46px";
+  }};
+  line-height: ${(props: ButtonProps) => {
+    if (props.large) return "2.8rem";
+    return "2.6rem";
+  }};
+  font-size: ${(props: ButtonProps) => {
+    if (props.large) return "2.1rem";
+    return "1.5rem";
+  }};
+  font-weight: ${(props: ButtonProps) => {
+    if (props.large) return "700";
+    return "500";
+  }};
+  height: ${(props: ButtonProps) => {
+    if (props.large) return "6.2rem";
+    return "3.8rem";
+  }};
+  border-radius: ${(props: ButtonProps) => {
+    if (props.large) return "3.1rem";
+    return "1.9rem";
+  }};
+  padding: 0
+    ${(props: ButtonProps) => {
+      if (props.large) return "5rem";
+      return "2.6rem";
+    }};
+  margin-top: ${(props: ButtonProps) => {
+    if (props.hero) return "8rem";
+    return "0";
+  }};
 
   @media (max-width: 600px) {
     height: 4.8rem;
     border-radius: 2.4rem;
     font-size: 1.4rem;
     padding: 0 5.2rem;
-    margin-top: ${(props: ButtonProps) => (props.hero ? "4.5rem" : "0")};
+
+    margin-top: ${(props: ButtonProps) => {
+      if (props.hero) return "4.5rem";
+      return "0";
+    }};
   }
 `;
 
