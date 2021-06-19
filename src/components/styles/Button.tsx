7d24abec9f7f075665eb85bf13139401965a90e2
Chase Manning <me@chasemanning.co.nz>
2021-06-20 11:27:20 +1200

    :sparkles: add gradient border option to button

--- src/components/styles/Button.tsx
+++ src/components/styles/Button.tsx
@@ -1,7 +1,6 @@
 import { CircularProgress } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
-import GradientText from "./GradientText";
 
 type ButtonProps = {
   primary?: boolean;
@@ -12,11 +11,41 @@ type ButtonProps = {
 };
 
 const StyledButton = styled.button`
+  background-color: pink;
+  cursor: pointer;
+  transition: 0.5s;
+
+  background-image: ${(props: ButtonProps) => {
+    if (props.primary) return "linear-gradient(to right, transparent)";
+    return "linear-gradient(to right, var(--primary-gradient) 0%, var(--secondary-gradient) 100%);";
+  }};
+  padding: ${(props: ButtonProps) => {
+    if (props.primary) return "0";
+    return "1px";
+  }};
+  border-radius: ${(props: ButtonProps) => {
+    if (props.large) return "3.1rem";
+    if (props.square) return "1.4rem";
+    return "1.9rem";
+  }};
+  margin-top: ${(props: ButtonProps) => {
+    if (props.hero) return "8rem";
+    return "0";
+  }};
+
+  @media (max-width: 600px) {
+    margin-top: ${(props: ButtonProps) => {
+      if (props.hero) return "4.5rem";
+      return "0";
+    }};
+  }
+`;
+
+const Content = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  cursor: pointer;
   transition: 0.5s;
   background-size: 200% auto;
 
@@ -27,7 +56,7 @@ const StyledButton = styled.button`
   background-image: ${(props: ButtonProps) => {
     if (props.primary)
       return "linear-gradient(270deg, var(--primary-gradient) 0%, var(--secondary-gradient) 50%, var(--primary-gradient) 100%);";
-    return "linear-gradient(to right, transparent)";
+    return "linear-gradient(to right, var(--bg), var(--bg))";
   }};
   height: ${(props: ButtonProps) => {
     if (props.large) return "6.2rem";
@@ -45,16 +74,8 @@ const StyledButton = styled.button`
       if (props.square) return "3rem";
       return "2.6rem";
     }};
-  margin-top: ${(props: ButtonProps) => {
-    if (props.hero) return "8rem";
-    return "0";
-  }};
 
   @media (max-width: 600px) {
-    margin-top: ${(props: ButtonProps) => {
-      if (props.hero) return "4.5rem";
-      return "0";
-    }};
     height: ${(props: ButtonProps) => {
       if (props.square) return "5.6rem";
       return "4.8rem";
@@ -137,28 +158,34 @@ const Button = (props: Props) => {
   return (
     <StyledButton
       type={props.submit ? "submit" : "button"}
-      onClick={() => {
-        if (!props.loading && props.click) props.click();
-      }}
       primary={props.primary}
       hero={props.hero}
       large={props.large}
       square={props.square}
     >
-      {props.loading && (
-        <ProgressContainer>
-          <CircularProgress size={props.large ? 31 : 25} />
-        </ProgressContainer>
-      )}
-      <Text
+      <Content
+        onClick={() => {
+          if (!props.loading && props.click) props.click();
+        }}
         primary={props.primary}
-        hero={props.hero}
         large={props.large}
         square={props.square}
-        loading={props.loading}
       >
-        {props.text}
-      </Text>
+        {props.loading && (
+          <ProgressContainer>
+            <CircularProgress size={props.large ? 31 : 25} />
+          </ProgressContainer>
+        )}
+        <Text
+          primary={props.primary}
+          hero={props.hero}
+          large={props.large}
+          square={props.square}
+          loading={props.loading}
+        >
+          {props.text}
+        </Text>
+      </Content>
     </StyledButton>
   );
 };
