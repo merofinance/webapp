64584f4997ad4e68b9cf69b6b3cfd5ee4f47bf72
Chase Manning <me@chasemanning.co.nz>
2021-06-19 14:53:54 +1200

    :bug: fix pools url case sensitivity issue

--- src/features/pool/selectors.ts
+++ src/features/pool/selectors.ts
@@ -3,7 +3,9 @@ import { RootState } from "../../app/store";
 import { Optional, Pool } from "../../lib/types";
 
 export function selectPool(poolName: string): (state: RootState) => Optional<Pool> {
-  return (state: RootState) => state.pools.pools.find((p) => p.lpToken.symbol === poolName) || null;
+  return (state: RootState) =>
+    state.pools.pools.find((p) => p.lpToken.symbol.toLowerCase() === poolName.toLowerCase()) ||
+    null;
 }
 
 export function getAddress(addressOrPool: string | Optional<Pool>): Optional<string> {
