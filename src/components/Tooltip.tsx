4d6194497b1c34b4bda7804e98b70bbdcf74f252
Chase Manning <me@chasemanning.co.nz>
2021-06-20 09:51:57 +1200

    :lipstick: style tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -5,7 +5,6 @@ import info from "../assets/ui/info.svg";
 const StyledTooltip = styled.div`
   position: relative;
   margin-left: 0.9rem;
-  z-index: 2;
   margin-top: 2px;
 `;
 
@@ -21,16 +20,34 @@ const ExitEvent = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(5, 1, 32, 0.5);
+  z-index: 1;
 `;
 
 const Popup = styled.div`
   position: absolute;
   top: 100%;
   left: 50%;
-  transform: translateX(-50%);
+  transform: translate(-50%, 1rem);
   width: 300px;
-  height: 40px;
-  background-color: green;
+  max-width: 30rem;
+  font-size: 1.1rem;
+  background-color: #433b6b;
+  box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14),
+    0px 3px 14px 2px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+  padding: 0.6rem 0.8rem;
+  z-index: 2;
+
+  :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 10px;
+    height: 10px;
+    background-color: #433b6b;
+    transform: translate(-50%, -0.5rem) rotate(45deg);
+  }
 `;
 
 type Props = {
