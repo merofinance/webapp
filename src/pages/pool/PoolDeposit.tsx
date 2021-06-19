20484148e51cea5ffc861e85b3c9ff964f25cc0a
Chase Manning <me@chasemanning.co.nz>
2021-06-20 10:49:31 +1200

    :sparkles: create pool deposit component

---
+++ src/pages/pool/PoolDeposit.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import ContentSection from "../../components/ContentSection";
+
+type Props = {
+  token: string;
+};
+
+const PoolDeposit = (props: Props) => {
+  return (
+    <ContentSection
+      header={`Deposit ${props.token.toUpperCase()}`}
+      statistics={[
+        {
+          header: "Your deposits",
+          value: "$130,000.00",
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        },
+        {
+          header: "Locked in position",
+          value: "$90,000.00",
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        },
+        {
+          header: "Rewards accrued",
+          value: "$14,000.00",
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        },
+      ]}
+      content={<p>deposit</p>}
+    />
+  );
+};
+
+export default PoolDeposit;
