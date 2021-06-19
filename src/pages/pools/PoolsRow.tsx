630b96e686a09e7da237a7639d43ab804e50127a
Chase Manning <me@chasemanning.co.nz>
2021-06-19 13:53:17 +1200

    :sparkles: create pools row component

---
+++ src/pages/pools/PoolsRow.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import chevron from "../../assets/ui/chevron.svg";
+import Asset, { AssetType } from "../../components/Asset";
+import GradientText from "../../components/styles/GradientText";
+
+const Row = styled.tr`
+  height: 7.2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 0.8rem;
+  background-color: #141128;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+  padding: 0 1.7rem;
+  cursor: pointer;
+`;
+
+const Data = styled.td`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 1.4rem;
+  letter-spacing: 0.15px;
+`;
+
+const Apy = styled(GradientText)`
+  font-weight: 900;
+  font-size: 1.6rem;
+  line-height: 2rem;
+  letter-spacing: 0.15px;
+`;
+
+const ChevronData = styled.td`
+  width: 2.4rem;
+`;
+
+const Chevron = styled.img`
+  height: 2.4rem;
+  width: 2.4rem;
+`;
+
+type Props = {
+  asset: AssetType;
+};
+
+const PoolsRow = (props: Props) => {
+  return (
+    <Row>
+      <Data>
+        <Asset asset={props.asset} />
+      </Data>
+      <Data>
+        <Apy>5.2%</Apy>
+      </Data>
+      <Data>$3.34m</Data>
+      <Data>$0.00</Data>
+      <ChevronData>
+        <Chevron src={chevron} />
+      </ChevronData>
+    </Row>
+  );
+};
+
+export default PoolsRow;
