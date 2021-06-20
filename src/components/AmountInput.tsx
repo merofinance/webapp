da963cb5a514a369a17f9db27e9acfd83b033d56
Chase Manning <me@chasemanning.co.nz>
2021-06-20 14:00:42 +1200

    :sparkles: create amount input component

---
+++ src/components/AmountInput.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "./styles/Button";
+
+const StyledAmountInput = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Available = styled.div`
+  width: 100%;
+  text-align: right;
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 2rem;
+  letter-spacing: 0.15px;
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  padding: 4px;
+  background: linear-gradient(to right, rgba(197, 50, 249, 0.25), rgba(50, 178, 229, 0.25));
+  border-radius: 4px;
+`;
+
+const InputBorder = styled.div`
+  padding: 2px;
+  background: linear-gradient(to right, rgb(197, 50, 249), rgb(50, 178, 229));
+  border-radius: 4px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 5.2rem;
+  background-color: #10092e;
+  padding: 0 1.2rem;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.15px;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  left: 1.6rem;
+  top: 0;
+  background-color: #10092e;
+  padding: 0 4px;
+  font-weight: 700;
+  font-size: 1.2rem;
+  line-height: 1.2;
+  letter-spacing: 0.15px;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 2.4rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+type Props = {
+  label: string;
+  max: number;
+};
+
+const AmountInput = (props: Props) => {
+  const [value, setValue] = useState("");
+
+  return (
+    <StyledAmountInput>
+      <Available>{`Available: ${props.max}`}</Available>
+      <InputContainer>
+        <InputBorder>
+          <Input type="number" placeholder={"0"} value={value} />
+        </InputBorder>
+        <Label>{props.label}</Label>
+        <ButtonContainer>
+          <Button primary small text="max" />
+        </ButtonContainer>
+      </InputContainer>
+    </StyledAmountInput>
+  );
+};
+
+export default AmountInput;
