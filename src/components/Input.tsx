20287db4ec151ba72a7ac45d6908a05b41f07c41
Chase Manning <me@chasemanning.co.nz>
2021-06-23 19:46:52 +1200

    :rocket: add hover and focus effects to border

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "./styles/Button";
 
-const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  margin: 0.6rem 0;
-`;
-
-const InputContainer = styled.div`
-  position: relative;
-  padding: 4px;
-  background: linear-gradient(to right, rgba(197, 50, 249, 0.25), rgba(50, 178, 229, 0.25));
-  border-radius: 4px;
-`;
-
-const InputBorder = styled.div`
-  padding: 2px;
-  background: linear-gradient(to right, rgb(197, 50, 249), rgb(50, 178, 229));
-  border-radius: 4px;
-`;
-
 type InputProps = {
+  focused?: boolean;
+  hover?: boolean;
   background?: string;
 };
 
-const StyledInput = styled.input`
+const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 5.2rem;
-  background-color: ${(props: InputProps) => props.background ?? "var(--bg)"};
+`;
+
+const Glow = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background: linear-gradient(to right, rgba(197, 50, 249, 0.25), rgba(50, 178, 229, 0.25));
+  border-radius: 1.8rem;
+
+  transition: all 0.3s;
+  width: ${(props: InputProps) => (props.focused ? "calc(100% + 12px)" : "100%")};
+  height: ${(props: InputProps) => (props.focused ? "calc(100% + 12px)" : "100%")};
+`;
+
+const Border = styled.div`
+  position: absolute;
+  width: 100%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  border-radius: ${(props: InputProps) => (props.hover || props.focused ? "1.6rem" : "1.5rem")};
+  width: ${(props: InputProps) =>
+    props.hover || props.focused ? "calc(100% + 4px)" : "calc(100% + 2px)"};
+  height: ${(props: InputProps) =>
+    props.hover || props.focused ? "calc(100% + 4px)" : "calc(100% + 2px)"};
+  background: ${(props: InputProps) =>
+    props.focused
+      ? "linear-gradient(to right, rgb(197, 50, 249), rgb(50, 178, 229))"
+      : "rgba(209, 209, 209, 1)"};
+`;
+
+const StyledInput = styled.input`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
   padding: 0 1.2rem;
   font-weight: 400;
   font-size: 1.6rem;
@@ -38,6 +58,7 @@ const StyledInput = styled.input`
   align-items: center;
   letter-spacing: 0.15px;
   -moz-appearance: textfield;
+  border-radius: 1.4rem;
 
   ::-webkit-outer-spin-button {
     display: none;
@@ -45,6 +66,8 @@ const StyledInput = styled.input`
   ::-webkit-inner-spin-button {
     display: none;
   }
+
+  background-color: ${(props: InputProps) => props.background ?? "var(--bg)"};
 `;
 
 const Label = styled.label`
@@ -78,24 +101,29 @@ type Props = {
 };
 
 const Input = (props: Props) => {
+  const [focused, setFocused] = useState(false);
+  const [hover, setHover] = useState(false);
+
   return (
-    <Container>
-      <InputContainer>
-        <InputBorder>
-          <StyledInput
-            type={props.type ?? "text"}
-            value={props.value}
-            onChange={(e) => props.onChange(e.target.value)}
-            background={props.background}
-          />
-        </InputBorder>
-        <Label>{props.label}</Label>
-        {props.buttonText && props.buttonAction && (
-          <ButtonContainer>
-            <Button primary small text={props.buttonText} click={() => props.buttonAction!()} />
-          </ButtonContainer>
-        )}
-      </InputContainer>
+    <Container onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+      <Glow focused={focused} />
+      <Border hover={hover} focused={focused} />
+      <StyledInput
+        type={props.type ?? "text"}
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+        background={props.background}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        hover={hover}
+        focused={focused}
+      />
+      <Label>{props.label}</Label>
+      {props.buttonText && props.buttonAction && (
+        <ButtonContainer>
+          <Button primary small text={props.buttonText} click={() => props.buttonAction!()} />
+        </ButtonContainer>
+      )}
     </Container>
   );
 };
