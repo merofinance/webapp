151c701aa76c25957bee4d34be763d90e7562116
Chase Manning <me@chasemanning.co.nz>
2021-06-23 18:56:30 +1200

    :art: change amount input to use input component

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import AmountSlider from "./AmountSlider";
-import Button from "./styles/Button";
+import Input from "./Input";
 
 const StyledAmountInput = styled.div`
   width: 100%;
@@ -21,60 +21,6 @@ const Available = styled.div`
   padding-right: 0.5rem;
 `;
 
-const InputContainer = styled.div`
-  position: relative;
-  padding: 4px;
-  background: linear-gradient(to right, rgba(197, 50, 249, 0.25), rgba(50, 178, 229, 0.25));
-  border-radius: 4px;
-  margin-bottom: 1.4rem;
-`;
-
-const InputBorder = styled.div`
-  padding: 2px;
-  background: linear-gradient(to right, rgb(197, 50, 249), rgb(50, 178, 229));
-  border-radius: 4px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  height: 5.2rem;
-  background-color: #10092e;
-  padding: 0 1.2rem;
-  font-weight: 400;
-  font-size: 1.6rem;
-  line-height: 1.9rem;
-  display: flex;
-  align-items: center;
-  letter-spacing: 0.15px;
-  -moz-appearance: textfield;
-
-  ::-webkit-outer-spin-button {
-    display: none;
-  }
-  ::-webkit-inner-spin-button {
-    display: none;
-  }
-`;
-
-const Label = styled.label`
-  position: absolute;
-  left: 1.6rem;
-  top: -2px;
-  background-color: #10092e;
-  padding: 0 4px;
-  font-weight: 700;
-  font-size: 1.2rem;
-  line-height: 1.2;
-  letter-spacing: 0.15px;
-`;
-
-const ButtonContainer = styled.div`
-  position: absolute;
-  right: 2.4rem;
-  top: 50%;
-  transform: translateY(-50%);
-`;
-
 type Props = {
   label: string;
   max: number;
@@ -86,20 +32,14 @@ const AmountInput = (props: Props) => {
   return (
     <StyledAmountInput>
       <Available>{`Available: ${props.max}`}</Available>
-      <InputContainer>
-        <InputBorder>
-          <Input
-            type="number"
-            placeholder={"0"}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </InputBorder>
-        <Label>{props.label}</Label>
-        <ButtonContainer>
-          <Button primary small text="max" click={() => setValue(props.max.toString())} />
-        </ButtonContainer>
-      </InputContainer>
+      <Input
+        valid={true}
+        label={props.label}
+        value={value}
+        onChange={(v: string) => setValue(v)}
+        buttonText="max"
+        buttonAction={() => setValue(props.max.toString())}
+      />
       <AmountSlider value={value} max={props.max} setValue={(v: string) => setValue(v)} />
     </StyledAmountInput>
   );
