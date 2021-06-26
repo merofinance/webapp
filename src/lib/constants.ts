4e236146ed87752e5ffe927b6085fb199e31f1f3
Chase Manning <me@chasemanning.co.nz>
2021-06-27 10:31:10 +1200

    :sparkles: create placholder tooltip constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,5 @@ export const ETH_DECIMALS = 18;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
 export const LIVE: boolean = true;
+export const PLACEHOLDER_TOOLTIP =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris";
