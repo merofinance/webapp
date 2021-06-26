f34efbedfe471f044c9bec68cb8610fcaeee82d2
Chase Manning <me@chasemanning.co.nz>
2021-06-26 17:05:55 +1200

    :rocket: add inputs to new position

--- src/pages/pool/NewPosition.tsx
+++ src/pages/pool/NewPosition.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Dropdown from "../../components/Dropdown";
+import Button from "../../components/styles/Button";
 
 const Border = styled.div`
   width: 100%;
@@ -15,11 +16,10 @@ const StyledNewPosition = styled.div`
   display: flex;
   background: linear-gradient(to right, #451467, #173d63);
   border-radius: 1.2rem;
-  padding: 1.7rem 2rem;
+  padding: 0.9rem 2rem;
 
-  div:last-child {
+  > div:last-child {
     justify-content: flex-end;
-    flex: 0.5;
   }
 `;
 
@@ -33,8 +33,45 @@ const Value = styled.div`
   align-items: center;
 `;
 
+const InputBorder = styled.div`
+  background: linear-gradient(to right, #c532f9, #32b2e5);
+  padding: 1px;
+  border-radius: 7px;
+  height: 3.2rem;
+  width: 67%;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  background-color: #252140;
+  border-radius: 6px;
+  height: 100%;
+  padding: 0 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.6rem;
+  letter-spacing: 0.15px;
+  font-weight: 400;
+
+  ::-webkit-outer-spin-button {
+    display: none;
+  }
+  ::-webkit-inner-spin-button {
+    display: none;
+  }
+
+  ::placeholder {
+    color: rgba(255, 255, 255, 0.4);
+  }
+`;
+
 const NewPosition = () => {
   const [protocol, setProtocol] = useState("");
+  const [borrower, setBorrower] = useState("");
+  const [threshold, setThreshold] = useState("");
+  const [single, setSingle] = useState("");
+  const [total, setTotal] = useState("");
 
   return (
     <Border>
@@ -46,11 +83,49 @@ const NewPosition = () => {
             setValue={(v: string) => setProtocol(v)}
           />
         </Value>
-        <Value>meow</Value>
-        <Value>meow</Value>
-        <Value>meow</Value>
-        <Value>meow</Value>
-        <Value>meow</Value>
+        <Value>
+          <InputBorder>
+            <Input
+              type="text"
+              value={borrower}
+              placeholder="0"
+              onChange={(e) => setBorrower(e.target.value)}
+            />
+          </InputBorder>
+        </Value>
+        <Value>
+          <InputBorder>
+            <Input
+              type="number"
+              value={threshold}
+              placeholder="0"
+              onChange={(e) => setThreshold(e.target.value)}
+            />
+          </InputBorder>
+        </Value>
+        <Value>
+          <InputBorder>
+            <Input
+              type="number"
+              value={single}
+              placeholder="0"
+              onChange={(e) => setSingle(e.target.value)}
+            />
+          </InputBorder>
+        </Value>
+        <Value>
+          <InputBorder>
+            <Input
+              type="number"
+              value={total}
+              placeholder="0"
+              onChange={(e) => setTotal(e.target.value)}
+            />
+          </InputBorder>
+        </Value>
+        <Value>
+          <Button primary disabled text="Create 1/2" />
+        </Value>
       </StyledNewPosition>
     </Border>
   );
