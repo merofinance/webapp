d598cd4710b16f25725a6f6455da928173b401d5
Chase Manning <me@chasemanning.co.nz>
2021-06-26 20:37:52 +1200

    :art: change new position to use new position input

--- src/pages/pool/NewPosition.tsx
+++ src/pages/pool/NewPosition.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Dropdown from "../../components/Dropdown";
 import Button from "../../components/styles/Button";
 import NewPositionConfirmation from "./NewPositionConfirmation";
+import NewPositionInput from "./NewPositionInput";
 
 const Border = styled.div`
   width: 100%;
@@ -34,39 +35,6 @@ export const Value = styled.div`
   align-items: center;
 `;
 
-const InputBorder = styled.div`
-  background: linear-gradient(to right, #c532f9, #32b2e5);
-  padding: 1px;
-  border-radius: 7px;
-  height: 3.2rem;
-  width: 67%;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  background-color: #252140;
-  border-radius: 6px;
-  height: 100%;
-  padding: 0 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.6rem;
-  letter-spacing: 0.15px;
-  font-weight: 400;
-
-  ::-webkit-outer-spin-button {
-    display: none;
-  }
-  ::-webkit-inner-spin-button {
-    display: none;
-  }
-
-  ::placeholder {
-    color: rgba(255, 255, 255, 0.4);
-  }
-`;
-
 const NewPosition = () => {
   const [protocol, setProtocol] = useState("");
   const [borrower, setBorrower] = useState("");
@@ -86,46 +54,14 @@ const NewPosition = () => {
             setValue={(v: string) => setProtocol(v)}
           />
         </Value>
-        <Value>
-          <InputBorder>
-            <Input
-              type="text"
-              value={borrower}
-              placeholder="0"
-              onChange={(e) => setBorrower(e.target.value)}
-            />
-          </InputBorder>
-        </Value>
-        <Value>
-          <InputBorder>
-            <Input
-              type="number"
-              value={threshold}
-              placeholder="0"
-              onChange={(e) => setThreshold(e.target.value)}
-            />
-          </InputBorder>
-        </Value>
-        <Value>
-          <InputBorder>
-            <Input
-              type="number"
-              value={single}
-              placeholder="0"
-              onChange={(e) => setSingle(e.target.value)}
-            />
-          </InputBorder>
-        </Value>
-        <Value>
-          <InputBorder>
-            <Input
-              type="number"
-              value={total}
-              placeholder="0"
-              onChange={(e) => setTotal(e.target.value)}
-            />
-          </InputBorder>
-        </Value>
+        <NewPositionInput type="text" value={borrower} setValue={(v: string) => setBorrower(v)} />
+        <NewPositionInput
+          type="number"
+          value={threshold}
+          setValue={(v: string) => setThreshold(v)}
+        />
+        <NewPositionInput type="number" value={single} setValue={(v: string) => setSingle(v)} />
+        <NewPositionInput type="number" value={total} setValue={(v: string) => setTotal(v)} />
         <Value>
           <Button
             primary
