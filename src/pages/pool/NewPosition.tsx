9560da34f39d85c16f8df4b50e854b5ac72fb891
Chase Manning <me@chasemanning.co.nz>
2021-06-27 09:24:34 +1200

    :rocket: add new position input validations

--- src/pages/pool/NewPosition.tsx
+++ src/pages/pool/NewPosition.tsx
@@ -4,6 +4,7 @@ import Dropdown from "../../components/Dropdown";
 import Button from "../../components/styles/Button";
 import NewPositionConfirmation from "./NewPositionConfirmation";
 import NewPositionInput from "./NewPositionInput";
+import { PositionType } from "./PoolPositions";
 
 const Border = styled.div`
   width: 100%;
@@ -46,25 +47,71 @@ export const Value = styled.div`
 `;
 
 const NewPosition = () => {
+  // Protocol
   const [protocol, setProtocol] = useState("");
   const [protocolError, setProtocolError] = useState("");
+  const validateProtocol = (value: string) => {
+    if (!value) setProtocolError("Select Protocol");
+    else setProtocolError("");
+  };
 
+  // Borrower
   const [borrower, setBorrower] = useState("");
   const [borrowerError, setBorrowerError] = useState("");
+  const validateBorrower = (value: string) => {
+    if (value.length !== 42) setBorrowerError("Invalid Address");
+    else setBorrowerError("");
+  };
 
+  // Threshold
   const [threshold, setThreshold] = useState("");
   const [thresholdError, setThresholdError] = useState("");
+  const validateThreshold = (value: string) => {
+    const number = Number(value);
+    if (number <= 1) setThresholdError("Must be above 1");
+    else setBorrowerError("");
+  };
 
+  // Single Topup
   const [single, setSingle] = useState("");
   const [singleError, setSingleError] = useState("");
+  const validateSingle = (value: string) => {
+    const number = Number(value);
+    if (number <= 0) setSingleError("Must be positive number");
+    else if (totalError && number > Number(totalError))
+      setTotalError("Must be less than total topup");
+    else setSingleError("");
+  };
 
+  // Total Topup
   const [total, setTotal] = useState("");
   const [totalError, setTotalError] = useState("");
+  const validateTotal = (value: string) => {
+    const number = Number(value);
+    if (number <= 0) setTotalError("Must be positive number");
+    else if (singleError && number < Number(singleError))
+      setTotalError("Must be greater than single topup");
+    else setTotalError("");
+  };
 
   const [confirming, setConfirming] = useState(false);
   const [approved, setApproved] = useState(false);
 
-  const hasError = protocolError || borrowerError || thresholdError || singleError || totalError;
+  const hasError = !!(
+    protocolError ||
+    borrowerError ||
+    thresholdError ||
+    singleError ||
+    totalError
+  );
+
+  const position: PositionType = {
+    protocol,
+    borrower,
+    threshold: Number(threshold),
+    single: Number(single),
+    total: Number(total),
+  };
 
   return (
     <Border>
@@ -74,37 +121,52 @@ const NewPosition = () => {
             <Dropdown
               value={protocol}
               options={["aave", "compound"]}
-              setValue={(v: string) => setProtocol(v)}
+              setValue={(v: string) => {
+                validateProtocol(v);
+                setProtocol(v);
+              }}
             />
           </Value>
           <NewPositionInput
             type="text"
             value={borrower}
-            setValue={(v: string) => setBorrower(v)}
+            setValue={(v: string) => {
+              validateBorrower(v);
+              setBorrower(v);
+            }}
             error={borrowerError}
           />
           <NewPositionInput
             type="number"
             value={threshold}
-            setValue={(v: string) => setThreshold(v)}
+            setValue={(v: string) => {
+              validateThreshold(v);
+              setThreshold(v);
+            }}
             error={thresholdError}
           />
           <NewPositionInput
             type="number"
             value={single}
-            setValue={(v: string) => setSingle(v)}
+            setValue={(v: string) => {
+              validateSingle(v);
+              setSingle(v);
+            }}
             error={singleError}
           />
           <NewPositionInput
             type="number"
             value={total}
-            setValue={(v: string) => setTotal(v)}
+            setValue={(v: string) => {
+              validateTotal(v);
+              setTotal(v);
+            }}
             error={totalError}
           />
           <Value>
             <Button
               primary
-              disabled={!(protocol && borrower && threshold && single && total)}
+              disabled={!(protocol && borrower && threshold && single && total) || hasError}
               text={approved ? "create 2/2" : "approve 1/2"}
               click={() => {
                 if (approved) setConfirming(true);
@@ -118,13 +180,7 @@ const NewPosition = () => {
       <NewPositionConfirmation
         show={confirming}
         close={() => setConfirming(false)}
-        position={{
-          protocol,
-          borrower,
-          threshold: Number(threshold),
-          single: Number(single),
-          total: Number(total),
-        }}
+        position={position}
       />
     </Border>
   );
