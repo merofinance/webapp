038d70f475bedbd5d7afa7a6d6a9e93c86ff1406
Chase Manning <me@chasemanning.co.nz>
2021-06-27 09:24:23 +1200

    :wrench: set site live

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@ export const ETH_DUMMY_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 export const ETH_DECIMALS = 18;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
-export const LIVE: boolean = false;
+export const LIVE: boolean = true;
