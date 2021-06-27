fbaf7f64fa8083fbf45e6e96bf3372a7b79ecc6b
Chase Manning <me@chasemanning.co.nz>
2021-06-27 16:24:26 +1200

    :rocket: add pools to preview

--- src/pages/landing/Preview.tsx
+++ src/pages/landing/Preview.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PoolsRow from "../pools/PoolsRow";
 
 const StyledPreview = styled.div`
   width: 100%;
@@ -10,8 +11,41 @@ const StyledPreview = styled.div`
   }
 `;
 
+const Table = styled.table`
+  width: 100%;
+`;
+
+const HeaderRow = styled.tr`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1.7rem;
+`;
+
+const Header = styled.th`
+  flex: 1;
+  text-align: left;
+  font-weight: 700;
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+  letter-spacing: 0.15px;
+  opacity: 0.6;
+`;
+
 const Preview = () => {
-  return <StyledPreview>preview</StyledPreview>;
+  return (
+    <StyledPreview>
+      <Table>
+        <HeaderRow>
+          <Header>Asset</Header>
+          <Header>APY</Header>
+          <Header>TVL</Header>
+        </HeaderRow>
+        <PoolsRow preview asset={"eth"} />
+        <PoolsRow preview asset={"usdc"} />
+        <PoolsRow preview asset={"dai"} />
+      </Table>
+    </StyledPreview>
+  );
 };
 
 export default Preview;
