cf2b68c8a95df7a7074932a045f95760ebd4caeb
Chase Manning <me@chasemanning.co.nz>
2021-07-03 15:49:10 +1200

    :wrench: update eth dummy address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from "ethers";
 
-export const ETH_DUMMY_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const ETH_DUMMY_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ETH_DECIMALS = 18;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
