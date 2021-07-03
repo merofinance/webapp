b2d04c30874c47ca502899e764b39dce4d1962f2
Chase Manning <me@chasemanning.co.nz>
2021-07-03 17:23:49 +1200

    :sparkles: add deposits and positions to statistics

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { selectPools, selectPrices } from "../features/pools-list/poolsListSlice";
+import { deposit, selectBalances } from "../features/user/userSlice";
+import { Pool } from "../lib";
 import Statistic, { StatisticType } from "./Statistic";
 
 const StyledStatistics = styled.div`
@@ -10,17 +14,51 @@ const StyledStatistics = styled.div`
 `;
 
 type Props = {
-  header: string;
-  statistics: StatisticType[];
-  content: JSX.Element;
+  pool?: Pool;
 };
 
-const Statistics = (props: Props) => {
+const Statistics = ({ pool }: Props) => {
+  const pools = useSelector(selectPools);
+  const prices = useSelector(selectPrices);
+  const balances = useSelector(selectBalances);
+
+  const getBalance = (pool: Pool) => balances[pool.lpToken.address] || 0;
+  const getPrice = (pool: Pool) => prices[pool.underlying.symbol] || 0;
+
+  const deposits = pool
+    ? getBalance(pool) * getPrice(pool)
+    : pools.reduce((a: number, b: Pool) => a + getBalance(b) * getPrice(b), 0);
+
+  const locked = pool
+    ? pool.totalAssets * getPrice(pool)
+    : pools.reduce((a: number, b: Pool) => a + b.totalAssets * getPrice(b), 0);
+
   return (
     <StyledStatistics>
-      {props.statistics.map((statistic: StatisticType) => (
-        <Statistic statistic={statistic} />
-      ))}
+      <Statistic
+        statistic={{
+          header: "Your deposits",
+          value: `$${deposits.toLocaleString()}`,
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        }}
+      />
+      <Statistic
+        statistic={{
+          header: "Locked in position",
+          value: `$${locked.toLocaleString()}`,
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        }}
+      />
+      <Statistic
+        statistic={{
+          header: "Rewards accrued",
+          value: "$0.00",
+          tooltip:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+        }}
+      />
     </StyledStatistics>
   );
 };
