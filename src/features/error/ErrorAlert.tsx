4efcb70c76c7a2d2ffe268be2004a2ffc3904634
Chase Manning <me@chasemanning.co.nz>
2021-07-03 15:48:31 +1200

    :rocket: add error alert

--- src/features/error/ErrorAlert.tsx
+++ src/features/error/ErrorAlert.tsx
@@ -1,26 +1,21 @@
-import classnames from "classnames";
 import React from "react";
-import { Alert } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../app/store";
+import Popup from "../../components/Popup";
+import { Paragraph } from "../../components/styles/Headers";
 
-import styles from "./error.module.scss";
 import { selectError, setError } from "./errorSlice";
 
 export function ErrorAlert() {
   const error = useSelector(selectError);
   const dispatch: AppDispatch = useDispatch();
 
-  return error ? (
-    <Alert
-      className={classnames(["fixed-top", styles["global-alert"]])}
-      variant="danger"
-      onClose={() => dispatch(setError({ error: "" }))}
-      dismissible
-    >
-      An error has occured: "{error}". <br />
-      Please try again in a moment. If the error persists, you can contact us{" "}
-      <a href="https://t.me/backdchat">on Telegram</a>
-    </Alert>
-  ) : null;
+  return (
+    <Popup
+      show={!!error}
+      close={() => dispatch(setError({ error: "" }))}
+      header="an error occured"
+      content={<Paragraph>{error}</Paragraph>}
+    />
+  );
 }
