0fc77469afd4cb800e398bd365dce804e88ac028
Chase Manning <me@chasemanning.co.nz>
2021-07-05 21:57:21 +1200

    :wrench: change site to not live

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,6 +4,6 @@ export const ETH_DUMMY_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ETH_DECIMALS = 18;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
-export const LIVE: boolean = true;
+export const LIVE: boolean = false;
 export const PLACEHOLDER_TOOLTIP =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris";
