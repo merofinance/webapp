317cc5637997ac2d6efbd5c219a741f51eeb6d2a
Chase Manning <me@chasemanning.co.nz>
2021-07-10 19:53:58 +1200

    :art: change private route to directly trigger popup

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,16 +1,20 @@
 import React, { ReactChild } from "react";
 import { Route, RouteProps } from "react-router-dom";
-import RequireConnection from "./RequireConnection";
+import { useWeb3React } from "@web3-react/core";
+import { useHistory } from "react-router-dom";
+import WalletSelectPopup from "./WalletSelectPopup";
 
 type PrivateRouteProps = {
   children: ReactChild;
 } & RouteProps;
 
 export function PrivateRoute({ children }: PrivateRouteProps) {
+  const history = useHistory();
+  const { active } = useWeb3React();
   return (
     <Route>
       {children}
-      <RequireConnection />
+      <WalletSelectPopup show={!active} close={() => {}} exit={() => history.push("/")} />
     </Route>
   );
 }
