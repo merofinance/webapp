a890d5c6b3c58f1466ba95557b2074c7842f0010
Chase Manning <me@chasemanning.co.nz>
2021-07-12 09:57:15 +1200

    :sparkles: create stake page component

---
+++ src/pages/stake/StakePage.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Seo from "../../components/Seo";
+import StakeSummary from "./StakeSummary";
+import StakeAccordion from "./StakeAccodion";
+
+const pools: string[] = ["meow", "woof"];
+
+const StyledPoolsPage = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Headers = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0 1.6rem;
+  margin-bottom: 0.5rem;
+`;
+
+const Header = styled.div`
+  flex: 1;
+  font-weight: 700;
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+  letter-spacing: 0.15px;
+  color: rgba(255, 255, 255, 0.87);
+  opacity: 0.6;
+`;
+
+const ButtonHeader = styled.div`
+  flex: 1.8;
+`;
+
+const StakePage = () => {
+  const [openAccordions, setOpenAccordions] = useState<number[]>([0]);
+
+  const isOpen = (index: number): boolean => openAccordions.indexOf(index) >= 0;
+
+  const toggle = (index: number): void => {
+    const _openAccordions = [...openAccordions];
+    if (isOpen(index)) _openAccordions.splice(openAccordions.indexOf(index), 1);
+    else _openAccordions.push(index);
+    setOpenAccordions(_openAccordions);
+  };
+
+  return (
+    <StyledPoolsPage>
+      <Seo title="" description="" />
+      <StakeSummary />
+      <Headers>
+        <Header>Asset</Header>
+        <Header>Claimable (USD)</Header>
+        <Header>APR</Header>
+        <ButtonHeader />
+      </Headers>
+      {pools.map((pool: string, index: number) => (
+        <StakeAccordion
+          key={pool}
+          open={isOpen(index)}
+          toggle={() => toggle(index)}
+          rows={["meow", "woof", "cat"]}
+        />
+      ))}
+    </StyledPoolsPage>
+  );
+};
+
+export default StakePage;
