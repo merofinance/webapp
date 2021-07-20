204070269a5d28a17567dee458bd38f35f9d6c62
Chase Manning <me@chasemanning.co.nz>
2021-07-20 18:32:06 +1200

    :speech_balloon: add pool position tooltip copy

--- src/pages/pool/PoolPositions.tsx
+++ src/pages/pool/PoolPositions.tsx
@@ -21,23 +21,25 @@ type HeaderType = {
 const headers: HeaderType[] = [
   {
     label: "Protocol",
-    tooltip: PLACEHOLDER_TOOLTIP,
+    tooltip:
+      "The lending protocol on which the user is borrowing funds (currently compatible with Aave and Compound)",
   },
   {
     label: "Address",
-    tooltip: PLACEHOLDER_TOOLTIP,
+    tooltip:
+      "The address of the owner of the position to top up (e.g. if Alice is the borrower on Aave that should be topped up then this would be Alice’s address)",
   },
   {
     label: "Threshold",
-    tooltip: PLACEHOLDER_TOOLTIP,
+    tooltip: "The health factor threshold a collateral top up should occur at",
   },
   {
     label: "Single Top Up",
-    tooltip: PLACEHOLDER_TOOLTIP,
+    tooltip: "Amount of a single top up increment (e.g. top up increments of 2,500 DAI)",
   },
   {
     label: "Max Top Up",
-    tooltip: PLACEHOLDER_TOOLTIP,
+    tooltip: "Maximum top up amount (value of your liquidity allocated for top ups)",
   },
 ];
 
