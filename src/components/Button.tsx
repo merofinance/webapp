f605fad4131e09c6532c2308ade22e6169af51a0
Chase Manning <me@chasemanning.co.nz>
2021-07-25 19:07:26 +1200

    :bug: input button large on mobile

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -112,16 +112,16 @@ const Content = styled.div`
   @media (max-width: 600px) {
     height: ${(props: ButtonProps) => {
       if (props.square) return "5.6rem";
-      return "4.8rem";
+      if (props.primary) return "4.8rem";
     }};
     border-radius: ${(props: ButtonProps) => {
       if (props.square) return "1.4rem";
-      return "2.4rem";
+      if (props.primary) return "2.4rem";
     }};
     padding: 0
       ${(props: ButtonProps) => {
         if (props.square) return "1.8rem";
-        return "5.2rem";
+        if (props.primary) return "5.2rem";
       }};
   }
 `;
