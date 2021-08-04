a5db54219a31f73ef64505cf4225c913af68d118
Chase Manning <me@chasemanning.co.nz>
2021-08-04 17:52:49 +1200

    :iphone: mobile view for claim row

--- src/pages/claim/ClaimRow.tsx
+++ src/pages/claim/ClaimRow.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Asset from "../../components/Asset";
 import Button from "../../components/Button";
+import { useDevice } from "../../lib/hooks";
 
 interface ClaimRowProps {
   index: number;
@@ -14,42 +15,65 @@ const StyledClaimRow = styled.div`
 
   background-color: ${(props: ClaimRowProps) =>
     props.index % 2 === 0 ? "none" : "rgba(21, 14, 59, 1)"};
+
+  @media (max-width: 600px) {
+    justify-content: space-between;
+  }
 `;
 
 const Label = styled.div`
-  flex: 1;
   font-weight: 700;
-  font-size: 1.8rem;
   line-height: 1.7rem;
   letter-spacing: 0.15px;
+  white-space: nowrap;
+
+  @media (min-width: 601px) {
+    flex: 1;
+  }
+
+  font-size: 1.8rem;
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+  }
 `;
 
 const ValueContainer = styled.div`
-  flex: 2;
   display: flex;
   align-items: center;
+
+  @media (min-width: 601px) {
+    flex: 2;
+  }
 `;
 
 const ValueUsd = styled.div`
   font-weight: 400;
-  font-size: 1.8rem;
   line-height: 2.7rem;
   letter-spacing: 0.15px;
   opacity: 0.6;
+
+  font-size: 1.8rem;
   margin-left: 1.3rem;
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+    margin-left: 0.2rem;
+  }
 `;
 
 const EndContainer = styled.div`
-  flex: 1.8;
   display: flex;
   justify-content: flex-end;
 
-  button:first-child {
-    margin-right: 1.8rem;
-  }
+  @media (min-width: 601px) {
+    flex: 1.8;
 
-  button:last-child {
-    margin-right: 6rem;
+    button:first-child {
+      margin-right: 1.8rem;
+    }
+
+    button:last-child {
+      margin-right: 6rem;
+    }
   }
 `;
 
@@ -58,6 +82,8 @@ interface Props {
 }
 
 const ClaimRow = ({ index }: Props) => {
+  const { isMobile, isDesktop } = useDevice();
+
   return (
     <StyledClaimRow index={index}>
       <Label>ETH pool</Label>
@@ -69,14 +95,21 @@ const ClaimRow = ({ index }: Props) => {
             symbol: "DAI",
             decimals: 16,
           }}
+          hideIcon={isMobile}
           value={18}
           small
         />
         <ValueUsd>=$120.00</ValueUsd>
       </ValueContainer>
       <EndContainer>
-        <Button text="Claim" background="#100830" width="12rem" />
-        <Button primary text="Claim & Stake" width="18rem" />
+        <Button
+          text="Claim"
+          background="#100830"
+          width={isMobile ? "auto" : "12rem"}
+          small={isMobile}
+          primary={isMobile}
+        />
+        {isDesktop && <Button primary text="Claim & Stake" width="18rem" />}
       </EndContainer>
     </StyledClaimRow>
   );
