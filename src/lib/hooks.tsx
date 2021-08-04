36adeab4a160b1242684bb07c44d1a0daedf7e5f
Chase Manning <me@chasemanning.co.nz>
2021-08-04 22:28:17 +1200

    :art: change index of to includes

--- src/lib/hooks.tsx
+++ src/lib/hooks.tsx
@@ -6,7 +6,7 @@ const LIVE_CHAINS = [42, 1337];
 export const useIsLive = (): boolean => {
   const { chainId } = useWeb3React();
   if (!chainId) return false;
-  return LIVE_CHAINS.indexOf(chainId) > -1;
+  return LIVE_CHAINS.includes(chainId);
 };
 
 export const useDevice = () => {
