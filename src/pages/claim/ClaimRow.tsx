74fc5666a79fdc85ae8a685333080a14558f7deb
Chase Manning <me@chasemanning.co.nz>
2021-08-04 18:04:44 +1200

    :fire: remove redundant import

--- src/pages/claim/ClaimRow.tsx
+++ src/pages/claim/ClaimRow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Asset from "../../components/Asset";
 import Button from "../../components/Button";
