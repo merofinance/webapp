aef0cc37cdab60c127c232294972267c7fc87a85
Chase Manning <me@chasemanning.co.nz>
2021-08-04 17:57:02 +1200

    :iphone: accordion headers not aligned

--- src/components/AccordionChevron.tsx
+++ src/components/AccordionChevron.tsx
@@ -8,12 +8,17 @@ interface ArrowProps {
 
 const Arrow = styled.img`
   width: 1.2rem;
-  margin-right: 1.6rem;
-  margin-left: 3.2rem;
   transition: transform 0.3s;
   transform: ${(props: ArrowProps) => (props.open ? "rotate(0deg)" : "rotate(180deg)")};
   user-drag: none;
   user-select: none;
+
+  margin-right: 1.6rem;
+  margin-left: 3.2rem;
+  @media (max-width: 600px) {
+    margin-right: 1rem;
+    margin-left: 0;
+  }
 `;
 
 interface Props {
