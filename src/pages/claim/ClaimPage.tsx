ea0512dcc5c56704a067aa141c2b00d2653428b7
Chase Manning <me@chasemanning.co.nz>
2021-08-04 17:42:35 +1200

    :iphone: claim page mobile implementation

--- src/pages/claim/ClaimPage.tsx
+++ src/pages/claim/ClaimPage.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Seo from "../../components/Seo";
 import ClaimSummary from "./ClaimSummary";
 import ClaimAccordion from "./ClaimAccordion";
+import { useDevice } from "../../lib/hooks";
 
 const claims: string[] = ["test-claim-01", "test-claim-02"];
 
@@ -19,33 +20,50 @@ const Headers = styled.div`
   margin-bottom: 0.5rem;
 `;
 
+interface HeaderProps {
+  hideMobile?: boolean;
+}
+
 const Header = styled.div`
   flex: 1;
   font-weight: 700;
-  font-size: 1.4rem;
   line-height: 2.4rem;
   letter-spacing: 0.15px;
   color: rgba(255, 255, 255, 0.87);
   opacity: 0.6;
+
+  font-size: 1.4rem;
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+    display: ${(props: HeaderProps) => (props.hideMobile ? "none" : "flex")};
+  }
 `;
 
 const ButtonHeader = styled.div`
   flex: 1.8;
+  @media (max-width: 600px) {
+    flex: 0.3;
+  }
 `;
 
 const Note = styled.a`
   align-self: flex-end;
   margin-right: 0.5rem;
   font-weight: 500;
-  font-size: 1.4rem;
   line-height: 4.2rem;
   letter-spacing: 0.25px;
   transform: translateY(-4.5rem);
   cursor: pointer;
+
+  font-size: 1.4rem;
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+  }
 `;
 
 const ClaimPage = () => {
   const [openAccordions, setOpenAccordions] = useState<number[]>([0]);
+  const { isMobile } = useDevice();
 
   const isOpen = (index: number): boolean => openAccordions.indexOf(index) >= 0;
 
@@ -65,8 +83,8 @@ const ClaimPage = () => {
       <ClaimSummary />
       <Headers>
         <Header>Asset</Header>
-        <Header>Claimable (USD)</Header>
-        <Header>APR</Header>
+        <Header>{isMobile ? "Claimable" : "Claimable (USD)"}</Header>
+        <Header hideMobile>APR</Header>
         <ButtonHeader />
       </Headers>
       {claims.map((claim: string, index: number) => (
