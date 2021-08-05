63d245ac2753bc86089b793b305b36a6fa165909
Chase Manning <me@chasemanning.co.nz>
2021-08-05 23:49:05 +1200

    :sparkles: create split button component

---
+++ src/components/SplitButton.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core";
+
+const options = ["Claim & Stake", "Claim"];
+
+const useStyles = makeStyles({
+  right: {
+    background: "#36B0E6",
+    width: "3.8rem",
+    minWidth: "0",
+    //     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    //     border: 0,
+    //     borderRadius: 3,
+    //     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    //     color: "white",
+    //     height: 48,
+    //     padding: "0 30px",
+  },
+  left: {
+    whiteSpace: "nowrap",
+    background: "var(--gradient)",
+  },
+});
+
+export default function SplitButton() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (event: any, index: number) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Grid container direction="column" alignItems="center">
+      <Grid item xs={12}>
+        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+          <Button onClick={handleClick} className={classes.left}>
+            {options[selectedIndex]}
+          </Button>
+          <Button
+            size="small"
+            aria-controls={open ? "split-button-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-label="select merge strategy"
+            aria-haspopup="menu"
+            onClick={handleToggle}
+            className={classes.right}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+          style={{ zIndex: 100 }}
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin: placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList id="split-button-menu">
+                    {options.map((option, index) => (
+                      <MenuItem
+                        key={option}
+                        disabled={index === 2}
+                        selected={index === selectedIndex}
+                        onClick={(event) => handleMenuItemClick(event, index)}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Grid>
+    </Grid>
+  );
+}
