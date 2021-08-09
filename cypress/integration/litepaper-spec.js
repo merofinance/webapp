786253df336419535ecf84e6c045fdabb8dd06be
Chase Manning <me@chasemanning.co.nz>
2021-08-09 19:10:14 +1200

    :white_check_mark: add tests for litepaper page

---
+++ cypress/integration/litepaper-spec.js
@@ -0,0 +1,11 @@
+describe("Innitial Load", () => {
+  it("Should Load Litepaper Page", () => {
+    cy.visit("/litepaper");
+  });
+});
+
+describe("Page Content", () => {
+  it("Should have header", () => {
+    cy.get("h1").contains("Backd Litepaper");
+  });
+});
