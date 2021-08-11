eb7b722bb8375843e15684f308eb63da3f34fbf0
Chase Manning <me@chasemanning.co.nz>
2021-08-11 16:19:03 +1200

    :lipstick: increase spacing before stake inputs

--- src/pages/stake/StakeTokens.tsx
+++ src/pages/stake/StakeTokens.tsx
@@ -20,7 +20,7 @@ const Description = styled.div`
   font-size: 1.8rem;
   line-height: 2.6rem;
   letter-spacing: 0.46px;
-  margin-bottom: 2rem;
+  margin-bottom: 3.7rem;
 `;
 
 const MoreLink = styled(GradientLink)`
