600b5c7103da5b91665eb3c44bd61f1df00260bc
Chase Manning <me@chasemanning.co.nz>
2021-08-12 08:27:32 +1200

    :rocket: add icon and label to language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,10 +1,43 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-import Selector from "./Selector";
+import Selector, { SelectorOptionType } from "./Selector";
+
+import globe from "../assets/ui/globe.svg";
+
+const languageOptions: SelectorOptionType[] = [
+  {
+    value: "en",
+    label: "English",
+  },
+  {
+    value: "jp",
+    label: "日本語",
+  },
+];
 
 const StyledLanguageSelector = styled.button`
   display: flex;
+  align-items: center;
+  height: 3.6rem;
+  padding: 0 1.7rem;
+  border: solid 1px white;
+  border-radius: 0.7rem;
+`;
+
+const Globe = styled.img`
+  height: 1.7rem;
+`;
+
+const Label = styled.div`
+  font-size: 1.4rem;
+  font-weight: 500;
+  letter-spacing: 0.15px;
+  margin-left: 1.5rem;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
 `;
 
 const LanguageSelector = () => {
@@ -13,22 +46,18 @@ const LanguageSelector = () => {
 
   const [open, setOpen] = useState(false);
 
+  const activeLanguage = languageOptions.filter(
+    (option: SelectorOptionType) => option.value === i18n.language
+  )[0].label;
+
   return (
     <StyledLanguageSelector onClick={() => setOpen(true)} ref={anchorRef}>
-      {i18n.language}
+      <Globe src={globe} />
+      <Label>{activeLanguage}</Label>
       <Selector
         open={open}
         anchorRef={anchorRef}
-        options={[
-          {
-            value: "en",
-            label: "English",
-          },
-          {
-            value: "jp",
-            label: "日本語",
-          },
-        ]}
+        options={languageOptions}
         selected={i18n.language}
         close={() => setOpen(false)}
         select={(v: string) => i18n.changeLanguage(v)}
