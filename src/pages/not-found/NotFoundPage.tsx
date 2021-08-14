fcb8ff8d590197ca6d9bc6e2c2343bfbf0047bc9
Chase Manning <me@chasemanning.co.nz>
2021-08-14 21:21:45 +1200

    :sparkles: create not found page

---
+++ src/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledNotFoundPage = styled.div`
+  width: 100%;
+  display: flex;
+  margin: auto;
+  flex: 1;
+`;
+
+const NotFoundPage = () => {
+  return <StyledNotFoundPage>meow</StyledNotFoundPage>;
+};
+
+export default NotFoundPage;
