f0fc6b01b3a4b6395095a9cd7644486661941953
Chase Manning <me@chasemanning.co.nz>
2021-08-15 08:11:15 +1200

    :white_check_mark: add tests for not found page

---
+++ cypress/integration/not-found-page_spec.js
@@ -0,0 +1,61 @@
+describe("Innitial Load", () => {
+  it("Should Load Not Found Page", () => {
+    cy.visit("/askkasklaslkaskjl");
+  });
+});
+
+describe("Page Content", () => {
+  it("Should have number", () => {
+    cy.get("#not-found-number").contains("404");
+  });
+
+  it("Should have header", () => {
+    cy.get("h1").contains("Page could not be found.");
+  });
+});
+
+describe("Internal Links", () => {
+  it("Should Navigate to Home Page", () => {
+    cy.get("#not-found-home").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/");
+    });
+    cy.visit("/askkasklaslkaskjl");
+  });
+
+  it("Should Navigate to Pools Page", () => {
+    cy.get("#not-found-pools").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/connect");
+    });
+    cy.visit("/askkasklaslkaskjl");
+  });
+
+  it("Should Navigate to Claim Page", () => {
+    cy.get("#not-found-claim").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/claim");
+    });
+    cy.visit("/askkasklaslkaskjl");
+  });
+});
+
+describe("External Links", () => {
+  it("Should Have Support Link", () => {
+    cy.get("#not-found-support")
+      .should("have.attr", "target", "_blank")
+      .should("have.attr", "href", "https://discord.gg/jpGvaFV3Rv");
+  });
+
+  it("Should Have Docs Link", () => {
+    cy.get("#not-found-docs")
+      .should("have.attr", "target", "_blank")
+      .should("have.attr", "href", "https://docs.backd.fund/");
+  });
+});
+
+describe("Percy", () => {
+  it("Should Screenshot Not Found Page", () => {
+    cy.percySnapshot();
+  });
+});
