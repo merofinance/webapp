b50d04c660e193b3d2ebfc0db665f2603b7cfbef
Chase Manning <me@chasemanning.co.nz>
2021-08-15 18:33:08 +1200

    :iphone: stake description

--- src/pages/stake/StakeTokens.tsx
+++ src/pages/stake/StakeTokens.tsx
@@ -8,6 +8,7 @@ import MultiStepButtons from "../../components/MultiStepButtons";
 import { selectBalance } from "../../state/userSlice";
 import { Token } from "../../lib/types";
 import { GradientLink } from "../../styles/GradientText";
+import { useDevice } from "../../app/hooks/use-device";
 
 const StyledStakeTokens = styled.div`
   width: 100%;
@@ -17,18 +18,29 @@ const StyledStakeTokens = styled.div`
 
 const Description = styled.div`
   font-weight: 500;
+  letter-spacing: 0.46px;
+
   font-size: 1.8rem;
   line-height: 2.6rem;
-  letter-spacing: 0.46px;
   margin-bottom: 3.7rem;
+  @media (max-width: 600px) {
+    font-size: 1.3rem;
+    line-height: 1.9rem;
+    margin-bottom: 1.7rem;
+  }
 `;
 
 const MoreLink = styled(GradientLink)`
   font-weight: 500;
-  font-size: 1.8rem;
-  line-height: 2.6rem;
   letter-spacing: 0.46px;
   cursor: pointer;
+
+  font-size: 1.8rem;
+  line-height: 2.6rem;
+  @media (max-width: 600px) {
+    font-size: 1.3rem;
+    line-height: 1.9rem;
+  }
 `;
 
 const Content = styled.div`
@@ -45,6 +57,8 @@ type Props = {
 
 const StakeTokens = ({ token }: Props): JSX.Element => {
   const balance = useSelector(selectBalance(token.address));
+  const { isMobile } = useDevice();
+
   const [value, setValue] = useState("");
   const [approved, setApproved] = useState(false);
 
@@ -63,7 +77,10 @@ const StakeTokens = ({ token }: Props): JSX.Element => {
         Stake BKD and earn a share in x% of platform revenue, paid out in bkdDAI tokens.{" "}
         <MoreLink href="" target="_blank">
           More
-          <LaunchIcon style={{ fill: "var(--secondary)" }} />
+          <LaunchIcon
+            fontSize={isMobile ? "small" : "medium"}
+            style={{ fill: "var(--secondary)", transform: "translateY(2px)" }}
+          />
         </MoreLink>
       </Description>
       <Content>
