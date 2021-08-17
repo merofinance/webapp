53139308f816a04398f14c4f3bc8e7a7b3d544e3
Chase Manning <me@chasemanning.co.nz>
2021-08-17 17:18:50 +1200

    :white_check_mark: add tests for query parameter language setting

--- cypress/integration/language-spec.js
+++ cypress/integration/language-spec.js
@@ -1,16 +1,43 @@
-describe("Innitial Load", () => {
-  it("Should Load Home Page", () => {
+const isJapanese = () => {
+  cy.get("h1").contains("反応性流動資産");
+  cy.get("h3").contains(
+    "担保付ローンを清算から防ぐために設計された、管理者不在の利子生成プロトコルです。"
+  );
+  cy.get('[id="How It Works - 2"]').contains("利益獲得のための出資");
+};
+
+const isEnglish = () => {
+  cy.get("h1").contains("Reactive Liquidity");
+  cy.get("h3").contains(
+    "A trustless and interest generating protocol designed to prevent collateralized loans from becoming liquidatable."
+  );
+  cy.get('[id="How It Works - 2"]').contains("Stake to Earn Rewards");
+};
+
+describe("Setting Language with Query Parameter", () => {
+  it("Should Show English with no Parameter", () => {
     cy.visit("/");
+    isEnglish();
+  });
+
+  it("Should Show Japanese with JP Parameter", () => {
+    cy.visit("/?lang=jp");
+    isJapanese();
+  });
+
+  it("Should Show English with EN Parameter", () => {
+    cy.visit("/?lang=en");
+    isEnglish();
   });
 });
 
 describe("Language Switching", () => {
+  it("Should navigate to Langing Page", () => {
+    cy.visit("/");
+  });
+
   it("Should Show English by default", () => {
-    cy.get("h1").contains("Reactive Liquidity");
-    cy.get("h3").contains(
-      "A trustless and interest generating protocol designed to prevent collateralized loans from becoming liquidatable."
-    );
-    cy.get('[id="How It Works - 2"]').contains("Stake to Earn Rewards");
+    isEnglish();
   });
 
   it("Should Change Language to Japanese", () => {
@@ -19,11 +46,7 @@ describe("Language Switching", () => {
   });
 
   it("Should Show Japanese", () => {
-    cy.get("h1").contains("反応性流動資産");
-    cy.get("h3").contains(
-      "担保付ローンを清算から防ぐために設計された、管理者不在の利子生成プロトコルです。"
-    );
-    cy.get('[id="How It Works - 2"]').contains("利益獲得のための出資");
+    isJapanese();
   });
 
   it("Percy Should Screenshot Japanese Landing Page", () => {
@@ -36,10 +59,6 @@ describe("Language Switching", () => {
   });
 
   it("Should Show English again", () => {
-    cy.get("h1").contains("Reactive Liquidity");
-    cy.get("h3").contains(
-      "A trustless and interest generating protocol designed to prevent collateralized loans from becoming liquidatable."
-    );
-    cy.get('[id="How It Works - 2"]').contains("Stake to Earn Rewards");
+    isEnglish();
   });
 });
