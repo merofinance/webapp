f97a53f2f15b924279125177ec6827ce09e0f4e7
Chase Manning <me@chasemanning.co.nz>
2021-08-18 13:47:26 +1200

    :sparkles: create beta snackbar component

---
+++ src/components/BetaSnackbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { dismissBetaSnackbar, selectBetaSnackbarDismissed } from "../state/uiSlice";
+import Snackbar from "./Snackbar";
+
+const BetaSnackbar = () => {
+  const dispatch = useDispatch();
+  const dismissed = useSelector(selectBetaSnackbarDismissed);
+
+  return (
+    <Snackbar
+      show={!dismissed}
+      close={() => dispatch(dismissBetaSnackbar())}
+      text="Currently, all pools are in a shielded state. This allows for a maximum TVL of $1m per pool."
+      link={{
+        label: "Find out more.",
+        link: "",
+      }}
+    />
+  );
+};
+
+export default BetaSnackbar;
