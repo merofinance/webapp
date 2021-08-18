c7724562795284227079b38d634f3fa041438fac
Chase Manning <me@chasemanning.co.nz>
2021-08-18 13:46:39 +1200

    :sparkles: add info blue color

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
         --primary-gradient: #d127fa;
         --secondary: #32B2E5;
         --secondary-gradient: #24bde3;
+        --info: #03b8ff;
         --success: #4CAF50;
         --error: #F44336;
         --gradient: linear-gradient(to right, var(--primary), var(--secondary));
