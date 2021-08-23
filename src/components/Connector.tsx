c6daeb85ff4b11045d711bb848046153a25f9d43
Chase Manning <me@chasemanning.co.nz>
2021-08-23 17:06:58 +1200

    :rocket: add connection details to connector

--- src/components/Connector.tsx
+++ src/components/Connector.tsx
@@ -1,16 +1,17 @@
 import { useWeb3React } from "@web3-react/core";
 import React, { useEffect, useState } from "react";
-
-import WalletSelectPopup from "./WalletSelectPopup";
+import { useWeb3Updated } from "../app/hooks/use-web3-updated";
 import { injectedConnector } from "../app/web3";
+import ConnectionDetails from "./ConnectionDetails";
 import ConnectorDesktop from "./ConnectorDesktop";
 import ConnectorMobile from "./ConnectorMobile";
-import { useWeb3Updated } from "../app/hooks/use-web3-updated";
+import WalletSelectPopup from "./WalletSelectPopup";
 
 const Connector = (): JSX.Element => {
   const { active, activate } = useWeb3React();
   const updated = useWeb3Updated();
   const [connecting, setConnecting] = useState(false);
+  const [showingDetails, setShowingDetails] = useState(false);
 
   const autoConnect = async () => {
     const authorized = await injectedConnector.isAuthorized();
@@ -19,15 +20,21 @@ const Connector = (): JSX.Element => {
     }
   };
 
+  const onClick = () => {
+    if (active) setShowingDetails(true);
+    else setConnecting(true);
+  };
+
   useEffect(() => {
     autoConnect();
   }, [updated]);
 
   return (
     <>
-      <ConnectorDesktop connect={() => setConnecting(true)} />
-      <ConnectorMobile connect={() => setConnecting(true)} />
+      <ConnectorDesktop connect={onClick} />
+      <ConnectorMobile connect={onClick} />
       <WalletSelectPopup show={connecting} close={() => setConnecting(false)} />
+      <ConnectionDetails show={showingDetails} close={() => setShowingDetails(false)} />
     </>
   );
 };
