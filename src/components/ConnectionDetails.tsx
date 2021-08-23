3bdaf30e6bfeb28f82a7c94562c9ed2380b745b3
Chase Manning <me@chasemanning.co.nz>
2021-08-23 19:35:14 +1200

    :rocket: add wallet to connection details

--- src/components/ConnectionDetails.tsx
+++ src/components/ConnectionDetails.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Button from "./Button";
 
 import Popup from "./Popup";
 
@@ -10,12 +12,29 @@ const Content = styled.div`
   flex-direction: column;
 `;
 
+const WalletContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Wallet = styled.div`
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+  font-weight: 400;
+  letter-spacing: 0.15px;
+  opacity: 0.5;
+`;
+
 interface Props {
   show: boolean;
   close: () => void;
+  changeWallet: () => void;
+  wallet: string;
 }
 
-const ConnectionDetails = ({ show, close }: Props) => {
+const ConnectionDetails = ({ show, close, changeWallet, wallet }: Props) => {
   const { t } = useTranslation();
 
   return (
@@ -24,7 +43,19 @@ const ConnectionDetails = ({ show, close }: Props) => {
       show={show}
       close={close}
       header={t("walletConnect.details.header")}
-      content={<Content>meow</Content>}
+      content={
+        <Content>
+          <WalletContainer>
+            <Wallet>{t("walletConnect.details.connected", { wallet: t(wallet) })}</Wallet>
+            <Button
+              tiny
+              text={t("walletConnect.details.change")}
+              background="#252140"
+              click={changeWallet}
+            />
+          </WalletContainer>
+        </Content>
+      }
     />
   );
 };
