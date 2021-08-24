0cf4bd61f7ccfd09b15c8c18f7c64dd8a286c7ab
Chase Manning <me@chasemanning.co.nz>
2021-08-24 14:23:54 +1200

    :sparkles: create overview component

---
+++ src/components/Overview.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { GradientLink } from "../styles/GradientText";
+import InfoCard from "./InfoCard";
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Description = styled.div`
+  font-size: 1.5rem;
+  font-weight: 500;
+  line-height: 2rem;
+  letter-spacing: 0.46px;
+  opacity: 0.8;
+`;
+
+const Link = styled(GradientLink)`
+  font-size: 1.5rem;
+  font-weight: 500;
+  line-height: 2.1rem;
+  letter-spacing: 0.46px;
+  margin-top: 0.2rem;
+`;
+
+interface Props {
+  description: string;
+  link: string;
+}
+
+const Overview = ({ description, link }: Props) => {
+  return (
+    <InfoCard
+      collapsible
+      header="Overview"
+      content={
+        <Content>
+          <Description>{description}</Description>
+          <Link href={link} target="_blank" rel="noopener noreferrer">
+            Find out more in the docs.
+          </Link>
+        </Content>
+      }
+    />
+  );
+};
+
+export default Overview;
