d3f1deea416211ed8727f8d4bb000e3a18adb615
Chase Manning <me@chasemanning.co.nz>
2021-08-24 15:20:41 +1200

    :sparkles: add get etherscan url functions

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,6 +1,27 @@
+import { UnsupportedNetwork } from "../app/errors";
+import { etherscanUrls } from "./constants";
+
 export const changeNetwork = (chainId: number): void => {
   (window as any).ethereum.request({
     method: "wallet_switchEthereumChain",
     params: [{ chainId: `0x${chainId.toString(16)}` }],
   });
+};
+
+export const getEtherscanAddressLink = (chainId: number | undefined, address: string): string => {
+  return getEtherscanLink(chainId, "address", address);
+};
+
+export const getEtherscanTransactionLink = (
+  chainId: number | undefined,
+  transactionHash: string
+): string => {
+  return getEtherscanLink(chainId, "tx", transactionHash);
+};
+
+const getEtherscanLink = (chainId: number | undefined, type: string, data: string): string => {
+  if (!chainId) return "";
+  const url = etherscanUrls[chainId];
+  if (!url) throw new UnsupportedNetwork(chainId);
+  return `${url}${type}/${data}`;
 };
