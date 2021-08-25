66d2c4bc522a5fcd9833f1e106a80413540fc74e
Chase Manning <me@chasemanning.co.nz>
2021-08-25 21:25:49 +1200

    :rocket: add banner to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,32 +5,61 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo/logo.svg";
 import Connector from "./Connector";
 import NavItems from "./NavItems";
-import { useIsLive } from "../app/hooks/use-is-live";
 
 type HeaderProps = {
   isSticky: boolean;
 };
 
 const StyledHeader = styled.div`
-  position: relative;
   position: sticky;
   top: 0;
   left: 0;
   width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
   margin-bottom: 8rem;
   z-index: 1;
   transition: all 0.3s;
-  padding: 2.4rem 12.4rem;
 
   background-color: ${(props: HeaderProps) => (props.isSticky ? "#120e2c" : "transparent")};
   box-shadow: -4px 0px 4px rgba(0, 0, 0, ${(props: HeaderProps) => (props.isSticky ? "0.25" : "0")});
 
   @media (max-width: 600px) {
-    padding: 2.3rem 1.6rem;
     margin-bottom: 0;
+  }
+`;
+
+const Banner = styled.div`
+  width: 100%;
+  height: 5.2rem;
+  background: var(--gradient);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.6rem;
+  font-weight: 600;
+  line-height: 2.2rem;
+`;
+
+const BannerLink = styled.a`
+  font-size: 1.6rem;
+  font-weight: 600;
+  line-height: 2.2rem;
+  text-decoration: underline;
+  margin-left: 0.6rem;
+`;
+
+const Content = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.4rem 12.4rem;
+
+  @media (max-width: 600px) {
+    padding: 2.3rem 1.6rem;
   }
 
   @media (min-width: 601px) and (max-width: 1224px) {
@@ -65,11 +94,23 @@ const Header = (): JSX.Element => {
 
   return (
     <StyledHeader isSticky={scrollPosition > 40}>
-      <Link to="/">
-        <Logo src={logo} alt="Backd logo" />
-      </Link>
-      <NavItems />
-      <Connector />
+      <Banner>
+        🎉 Backd is now live on Kovan Testnet. Connect your wallet to begin or read
+        <BannerLink
+          href="https://backdfund.medium.com/backd-testnet-guide-16540e09c46"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          the blog post
+        </BannerLink>
+      </Banner>
+      <Content>
+        <Link to="/">
+          <Logo src={logo} alt="Backd logo" />
+        </Link>
+        <NavItems />
+        <Connector />
+      </Content>
     </StyledHeader>
   );
 };
