86f26ee9c71a157dc9afc6138e00c047828ddb6b
Chase Manning <me@chasemanning.co.nz>
2021-08-25 21:34:42 +1200

    :sparkles: create banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+// TODO Show only on landing page
+// Mobile support
+// Customisable and reusable
+
+const StyledBanner = styled.div`
+  width: 100%;
+  height: 5.2rem;
+  background: var(--gradient);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    height: auto;
+    padding: 0.9rem 0;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+const Text = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  line-height: 2.2rem;
+
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+    line-height: 1.8rem;
+  }
+`;
+
+const Link = styled.a`
+  font-size: 1.6rem;
+  font-weight: 600;
+  line-height: 2.2rem;
+  text-decoration: underline;
+  margin-left: 0.6rem;
+
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+    line-height: 1.8rem;
+    margin-left: 0.4rem;
+  }
+`;
+
+const Banner = (): JSX.Element => {
+  return (
+    <StyledBanner>
+      <Content>
+        <Text>🎉 Backd is now live on Kovan Testnet.</Text>
+        <Text>
+          Connect your wallet to begin or read
+          <Link
+            href="https://backdfund.medium.com/backd-testnet-guide-16540e09c46"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            the blog post
+          </Link>
+        </Text>
+      </Content>
+    </StyledBanner>
+  );
+};
+
+export default Banner;
