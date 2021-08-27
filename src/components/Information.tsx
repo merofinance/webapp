efe942683fbdc43fc21c3c952f0d84eb3d83efa4
Chase Manning <me@chasemanning.co.nz>
2021-08-27 19:34:52 +1200

    :sparkles: add row details option to information

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,29 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import { GradientLink } from "../styles/GradientText";
 import InfoCard from "./InfoCard";
 import Tooltip from "./Tooltip";
 
-interface OverviewRow {
+interface RowDetailType {
+  icon: string;
+  label: string;
+  link: string;
+}
+
+interface InformationRowType {
   label: string;
   tooltip: string;
   value: string;
+  details?: RowDetailType[];
 }
 
-const Statistics = styled.div`
+const StyledInformation = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
 `;
 
-const StatisticContainer = styled.div`
+const InformationRow = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
 
   margin-top: 1.4rem;
   @media (max-width: 1439px) {
     margin-top: 0.2rem;
   }
+`;
+
+interface RowProps {
+  isAccordion: boolean;
+}
+
+const InformationHeader = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  cursor: ${(props: RowProps) => (props.isAccordion ? "pointer" : "auto")};
 `;
 
 const LabelContainer = styled.div`
@@ -54,9 +74,34 @@ const Value = styled.div`
   }
 `;
 
+const AccordionContent = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-top: 0.8rem;
+`;
+
+const DetailItem = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const DetailIcon = styled.img`
+  width: 1.4rem;
+`;
+
+const DetailLabel = styled(GradientLink)`
+  font-size: 1.4rem;
+  font-weight: 500;
+  line-height: 2.8rem;
+  letter-spacing: 0.15px;
+  margin-left: 0.7rem;
+`;
+
 type Props = {
   header: string;
-  rows: OverviewRow[];
+  rows: InformationRowType[];
 };
 
 const Information = ({ header, rows }: Props): JSX.Element => {
@@ -64,17 +109,29 @@ const Information = ({ header, rows }: Props): JSX.Element => {
     <InfoCard
       header={header}
       content={
-        <Statistics>
-          {rows.map((row: OverviewRow) => (
-            <StatisticContainer key={row.label}>
-              <LabelContainer>
-                <Label>{row.label}</Label>
-                <Tooltip content={row.tooltip} />
-              </LabelContainer>
-              <Value>{row.value}</Value>
-            </StatisticContainer>
+        <StyledInformation>
+          {rows.map((row: InformationRowType) => (
+            <InformationRow key={row.label}>
+              <InformationHeader isAccordion={!!row.details}>
+                <LabelContainer>
+                  <Label>{row.label}</Label>
+                  <Tooltip content={row.tooltip} />
+                </LabelContainer>
+                <Value>{row.value}</Value>
+              </InformationHeader>
+              {row.details && (
+                <AccordionContent>
+                  {row.details.map((details: RowDetailType) => (
+                    <DetailItem>
+                      <DetailIcon src={details.icon} />
+                      <DetailLabel>{details.label}</DetailLabel>
+                    </DetailItem>
+                  ))}
+                </AccordionContent>
+              )}
+            </InformationRow>
           ))}
-        </Statistics>
+        </StyledInformation>
       }
     />
   );
