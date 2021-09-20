d2349698797aae370e71a382f844a44dff488be2
Chase Manning <me@chasemanning.co.nz>
2021-09-21 00:57:54 +1200

    :art: change uniswap action to png

--- src/pages/landing/FutureActions.tsx
+++ src/pages/landing/FutureActions.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import { Header2, Header4, Header5 } from "../../styles/Headers";
 import arrow from "../../assets/ui/pink-arrow.svg";
-import uniswap from "../../assets/actions/uniswap.svg";
+import uniswap from "../../assets/actions/uniswap.png";
 import limit from "../../assets/actions/limit.svg";
 import inbalance from "../../assets/actions/inbalance.svg";
 import nfts from "../../assets/actions/nfts.svg";
