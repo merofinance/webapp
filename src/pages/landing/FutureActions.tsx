dfb75984b5151066f96347b9ee0924487883a460
Chase Manning <me@chasemanning.co.nz>
2021-09-21 01:02:03 +1200

    :art: change imbalance asset to png

--- src/pages/landing/FutureActions.tsx
+++ src/pages/landing/FutureActions.tsx
@@ -6,7 +6,7 @@ import { Header2, Header4, Header5 } from "../../styles/Headers";
 import arrow from "../../assets/ui/pink-arrow.svg";
 import uniswap from "../../assets/actions/uniswap.png";
 import limit from "../../assets/actions/limit.png";
-import inbalance from "../../assets/actions/inbalance.svg";
+import inbalance from "../../assets/actions/inbalance.png";
 import nfts from "../../assets/actions/nfts.svg";
 import propose from "../../assets/actions/propose.svg";
 
