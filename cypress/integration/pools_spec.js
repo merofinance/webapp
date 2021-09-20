623f808e8bcf7eea9095140071a66a03dcfb07d5
Chase Manning <me@chasemanning.co.nz>
2021-09-20 23:59:52 +1200

    :white_check_mark: add tests for pools page

--- cypress/integration/pools_spec.js
+++ cypress/integration/pools_spec.js
@@ -2,7 +2,6 @@ import { percySnapshot } from "../support";
 import { INFURA_ID } from "../../src/lib/constants";
 import PrivateKeyProvider from "truffle-privatekey-provider";
 import Web3 from "web3";
-import { ethers } from "ethers";
 
 describe("Page Load", () => {
   it("Should Innitialise Web3", () => {
@@ -11,9 +10,33 @@ describe("Page Load", () => {
         "633361498918c6396a2d2e35de29285192cbed197ca3bcdacced769c21107bd7",
         `https://kovan.infura.io/v3/${INFURA_ID}`
       );
-      win.ethereum = new Web3(provider);
+      win.web3 = new Web3(provider);
     });
     cy.visit("/pools");
-    cy.contains("MetaMask").click();
+    cy.get('[id="walletConnect.wallets.metaMask"]').click();
+  });
+
+  it("Should load Pools", () => {
+    cy.get("#pool-row-bdai", { timeout: 30_000 }).should("be.visible");
+  });
+
+  it("Should navigate to Pool", () => {
+    cy.get("#pool-row-bdai", { timeout: 30_000 }).click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/pool/bDAI");
+    });
+  });
+
+  it("Should load balance", () => {
+    cy.get("#available-amount", { timeout: 30_000 }).contains(".", { timeout: 30_000 });
+  });
+
+  it("Should input value", () => {
+    cy.get("#amount-input").focus();
+    cy.get("#amount-input").type("10");
+  });
+
+  it("Should Deposit", () => {
+    cy.get("#action-button").click();
   });
 });
