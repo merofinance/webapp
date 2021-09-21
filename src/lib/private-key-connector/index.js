5b9f3728363bf1b4740d59311d8687c6a081d6b9
Chase Manning <me@chasemanning.co.nz>
2021-09-21 19:16:30 +1200

    :fire: remove redundant production connector

--- src/lib/private-key-connector/index.js
+++ src/lib/private-key-connector/index.js
@@ -1,7 +1,3 @@
 "use strict";
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./injected-connector.cjs.production.min.js");
-} else {
-  module.exports = require("./injected-connector.cjs.development.js");
-}
+module.exports = require("./injected-connector.cjs.development.js");
