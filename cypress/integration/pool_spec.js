9728c20918fd2aca4cc953aa402e28a500c3bfb4
Chase Manning <me@chasemanning.co.nz>
2021-09-21 23:26:41 +1200

    :white_check_mark: add test for pool header

--- cypress/integration/pool_spec.js
+++ cypress/integration/pool_spec.js
@@ -5,11 +5,19 @@ describe("Page Load", () => {
     initWeb3();
     cy.visit("/pools/pool/bDAI");
   });
+});
 
+describe("Default state", () => {
+  cy.get("#content-header").contains("Deposit DAI");
+});
+
+describe("Innitial Data", () => {
   it("Should load balance", () => {
     cy.get("#available-amount", { timeout: 30_000 }).contains(".", { timeout: 30_000 });
   });
+});
 
+describe("Depositing", () => {
   it("Should input value", () => {
     cy.get("#amount-input").focus();
     cy.get("#amount-input").type("10");
