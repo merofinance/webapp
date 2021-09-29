d05801ee0c0120422156245cb2907d1a40edac09
Chase Manning <me@chasemanning.co.nz>
2021-09-30 00:02:30 +1300

    :sparkles: create register topup loan component

---
+++ src/pages/actions/lending/RegisterTopupLoan.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { useHistory } from "react-router";
+import { useWeb3React } from "@web3-react/core";
+
+import ContentSection from "../../../components/ContentSection";
+import Button from "../../../components/Button";
+import BackButton from "../../../components/BackButton";
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  font-size: 2.2rem;
+  font-weight: 700;
+  letter-spacing: 0.25px;
+  margin-bottom: 3rem;
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 6rem;
+`;
+
+const RegisterTopupLoan = () => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const { account } = useWeb3React();
+
+  return (
+    <Container>
+      <BackButton />
+      <ContentSection
+        header={t("actions.register.header")}
+        nav="1/4"
+        content={
+          <Content>
+            <Header>{t("actions.register.choose")}</Header>
+            <ButtonContainer>
+              <Button
+                primary
+                medium
+                width="44%"
+                text={t("components.continue")}
+                click={() => history.push(`/actions/register/topup/${account}/Aave`)}
+              />
+            </ButtonContainer>
+          </Content>
+        }
+      />
+    </Container>
+  );
+};
+
+export default RegisterTopupLoan;
