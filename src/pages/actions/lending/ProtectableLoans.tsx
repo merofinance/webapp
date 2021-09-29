3ea488401c3ccb363668f5260842019d444ca3e8
Chase Manning <me@chasemanning.co.nz>
2021-09-29 22:13:24 +1300

    :sparkles: create protectable loans component

---
+++ src/pages/actions/lending/ProtectableLoans.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { ScaledNumber } from "../../../lib/scaled-number";
+import { selectAave, selectCompound } from "../../../state/lendingSlice";
+import ProtectableLoan from "./ProtectableLoan";
+
+const StyledProtectableLoans = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 1.4rem;
+  padding: 2.4rem 1.6rem;
+`;
+
+const Header = styled.div`
+  font-size: 2.4rem;
+  font-weight: 700;
+  line-height: 4.2rem;
+  letter-spacing: 0.25px;
+`;
+
+const SubHeader = styled.div`
+  font-size: 1.5rem;
+  font-weight: 500;
+  line-height: 2.1rem;
+  letter-spacing: 0.46px;
+  opacity: 0.8;
+`;
+
+const ProtectableLoans = () => {
+  const { t } = useTranslation();
+  const aave = useSelector(selectAave);
+  const compound = useSelector(selectCompound);
+
+  const hasAave = aave?.totalCollateralETH?.isZero && !aave.totalCollateralETH.isZero();
+  const hasCompound = compound?.totalCollateralETH?.isZero && !compound.totalCollateralETH.isZero();
+
+  if (!hasAave && !hasCompound) return <></>;
+
+  return (
+    <StyledProtectableLoans>
+      <Header>{t("actions.suggestions.topup.header")}</Header>
+      <SubHeader>{t("actions.suggestions.topup.subHeader")}</SubHeader>
+      {hasAave && <ProtectableLoan protocol="Aave" loan={aave} />}
+      {hasCompound && <ProtectableLoan protocol="Compound" loan={compound} />}
+    </StyledProtectableLoans>
+  );
+};
+
+export default ProtectableLoans;
