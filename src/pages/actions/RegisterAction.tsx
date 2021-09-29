0f1f916f335048c2f8e8e0f41b407c064e3d0fef
Chase Manning <me@chasemanning.co.nz>
2021-09-29 23:18:06 +1300

    :rocket: add note to register action option

--- src/pages/actions/RegisterAction.tsx
+++ src/pages/actions/RegisterAction.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+import { useHistory } from "react-router";
+
 import ContentSection from "../../components/ContentSection";
 import Radio, { RadioOptionType } from "../../components/Radio";
+import icon from "../../assets/logo/white-logo.svg";
+import Button from "../../components/Button";
 
 const Content = styled.div`
   width: 100%;
@@ -17,8 +21,34 @@ const Header = styled.div`
   margin-bottom: 3rem;
 `;
 
+const NoteContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 1.5rem;
+`;
+
+const BackdIcon = styled.img`
+  width: 2.2rem;
+  margin-right: 1rem;
+`;
+
+const Note = styled.div`
+  font-size: 1.6rem;
+  font-weight: 500;
+  line-height: 2.3rem;
+  opacity: 0.8;
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 6rem;
+`;
+
 const RegisterAction = () => {
   const { t } = useTranslation();
+  const history = useHistory();
   const [action, setAction] = useState("topup");
 
   const actions: RadioOptionType[] = [
@@ -32,7 +62,7 @@ const RegisterAction = () => {
       disabledText: t("components.comingSoon"),
     },
     {
-      value: "limitOrder",
+      value: "limit",
       label: t("actions.limitOrder.label"),
       disabledText: t("components.comingSoon"),
     },
@@ -59,6 +89,19 @@ const RegisterAction = () => {
             active={action}
             setOption={(value: string) => setAction(value)}
           />
+          <NoteContainer>
+            <BackdIcon src={icon} />
+            <Note>{t("actions.topup.description")}</Note>
+          </NoteContainer>
+          <ButtonContainer>
+            <Button
+              primary
+              medium
+              width="44%"
+              text={t("components.continue")}
+              click={() => history.push(`/actions/register/${action}`)}
+            />
+          </ButtonContainer>
         </Content>
       }
     />
