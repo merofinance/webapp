bb4045c90436496c9067b2c3a8752fd611dd6db5
Chase Manning <me@chasemanning.co.nz>
2021-10-01 00:53:53 +1300

    :sparkles: create row option component

---
+++ src/components/RowOption.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface ColumnType {
+  label: string;
+  value: string;
+}
+
+interface RowOptionProps {
+  active: boolean;
+}
+
+const StyledRowOption = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: ${(props: RowOptionProps) =>
+    props.active
+      ? "linear-gradient(to right, rgba(197, 50, 249, 0.1), rgba(50, 178, 229, 0.1))"
+      : "rgba(20, 17, 40, 1)"};
+  /* border: 1px solid rgba(255, 255, 255, 0.2); */
+  border-radius: 1.4rem;
+  padding: 1.3rem 1.4rem;
+  margin-top: 1rem;
+  cursor: pointer;
+`;
+
+const Column = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const Header = styled.div`
+  font-size: 1.2rem;
+  font-weight: 700;
+  letter-spacing: 0.2px;
+  opacity: 0.6;
+  margin-bottom: 0.5rem;
+`;
+
+const Value = styled.div`
+  font-size: 1.8rem;
+  font-weight: 700;
+  letter-spacing: 0.2px;
+`;
+
+interface Props {
+  columns: ColumnType[];
+  active: boolean;
+  select: () => void;
+}
+
+const RowOption = ({ columns, active, select }: Props) => {
+  return (
+    <StyledRowOption active={active} onClick={() => select()}>
+      {columns.map((column: ColumnType) => (
+        <Column>
+          <Header>{column.label}</Header>
+          <Value>{column.value}</Value>
+        </Column>
+      ))}
+    </StyledRowOption>
+  );
+};
+
+export default RowOption;
