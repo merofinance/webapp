42b6f4f6774b064f701e5e7fa43590707dcecbda
Chase Manning <me@chasemanning.co.nz>
2021-10-01 00:07:18 +1300

    :sparkles: add gradient option to radio

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-export type RadioOptionType = {
+export interface RadioOptionType {
   label: string;
   value: string;
   disabledText?: string;
-};
+}
 
 const StyledRadio = styled.div`
   position: relative;
@@ -13,23 +13,35 @@ const StyledRadio = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-type OptionProps = {
+interface OptionProps {
   active: boolean;
   disabled?: boolean;
-};
+  gradient?: boolean;
+}
 
 const RadioOption = styled.button`
   text-transform: capitalize;
   position: relative;
   width: 15.2rem;
-  height: 4.9rem;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
   transition: all 0.3s;
+  border-radius: 14px;
 
   cursor: ${(props: OptionProps) => (props.disabled ? "auto" : "pointer")};
+  background-color: ${(props: OptionProps) =>
+    !props.gradient ? "none" : props.active ? "rgba(197, 50, 249, 0.1)" : "transparent"};
+  border: solid 1px
+    ${(props: OptionProps) =>
+      !props.gradient
+        ? "none"
+        : props.active
+        ? "rgba(197, 50, 249, 0.7)"
+        : "rgba(197, 50, 249, 0.5)"};
+  margin-right: ${(props: OptionProps) => (props.gradient ? "1.6rem" : "0")};
+  height: ${(props: OptionProps) => (props.gradient ? "3.8rem" : "4.9rem")};
 
   :hover {
     opacity: ${(props: OptionProps) => (props.active || props.disabled ? "1" : "0.7")};
@@ -43,7 +55,6 @@ const RadioOption = styled.button`
     line-height: 26px;
     height: 3.8rem;
     width: 13.9rem;
-    border-radius: 14px;
     background-color: ${(props: OptionProps) =>
       props.active ? "rgba(197, 50, 249, 0.1)" : "transparent"};
     border: solid 1px
@@ -56,8 +67,14 @@ const RadioOption = styled.button`
 const RadioText = styled.div`
   font-size: 1.5rem;
   transition: color 0.3s;
-  font-weight: 700;
   color: ${(props: OptionProps) => (props.active ? "var(--main)" : "var(--sub)")};
+
+  background: ${(props: OptionProps) => (props.gradient ? "var(--gradient)" : "none")};
+  background-clip: ${(props: OptionProps) => (props.gradient ? "text" : "none")};
+  -webkit-background-clip: ${(props: OptionProps) => (props.gradient ? "text" : "none")};
+  -webkit-text-fill-color: ${(props: OptionProps) => (props.gradient ? "transparent" : "none")};
+  font-weight: ${(props: OptionProps) => (props.gradient ? "500" : "700")};
+  opacity: ${(props: OptionProps) => (!props.active && props.gradient ? "0.4" : "1")};
 
   @media (max-width: 600px) {
     font-size: 1.4rem;
@@ -70,9 +87,10 @@ const RadioText = styled.div`
   }
 `;
 
-type IndicatorProps = {
+interface IndicatorProps {
   activeIndex: number;
-};
+  gradient?: boolean;
+}
 
 const ActiveIndicator = styled.div`
   position: absolute;
@@ -84,6 +102,8 @@ const ActiveIndicator = styled.div`
   background-color: #322c4b;
   transition: transform 0.3s;
   transform: ${(props: IndicatorProps) => `translateX(${props.activeIndex * 15.2}rem)`};
+
+  display: ${(props: IndicatorProps) => (props.gradient ? "none" : "block")};
 
   @media (max-width: 600px) {
     display: none;
@@ -114,17 +134,19 @@ const HoverText = styled.div`
   white-space: nowrap;
 `;
 
-type Props = {
+interface Props {
   options: RadioOptionType[];
   active: string;
   setOption: (value: string) => void;
-};
+  gradient?: boolean;
+}
 
-const Radio = ({ options, active, setOption }: Props): JSX.Element => {
+const Radio = ({ options, active, setOption, gradient }: Props): JSX.Element => {
   return (
     <StyledRadio>
       <ActiveIndicator
         activeIndex={options.map((option: RadioOptionType) => option.value).indexOf(active)}
+        gradient={gradient}
       />
       {options.map((option: RadioOptionType) => (
         <RadioOption
@@ -133,8 +155,11 @@ const Radio = ({ options, active, setOption }: Props): JSX.Element => {
           onClick={() => setOption(option.value)}
           active={option.value === active}
           disabled={!!option.disabledText}
+          gradient={gradient}
         >
-          <RadioText active={option.value === active}>{option.label}</RadioText>
+          <RadioText active={option.value === active} gradient={gradient}>
+            {option.label}
+          </RadioText>
           {option.disabledText && (
             <HoverTextContainer>
               <HoverText>{option.disabledText}</HoverText>
