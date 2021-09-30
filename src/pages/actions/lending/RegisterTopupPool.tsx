aaa80727d1f2776f70189e6dc2f1892d88a64e58
Chase Manning <me@chasemanning.co.nz>
2021-10-01 02:31:37 +1300

    :rocket: add pool selection

--- src/pages/actions/lending/RegisterTopupPool.tsx
+++ src/pages/actions/lending/RegisterTopupPool.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 
 import ContentSection from "../../../components/ContentSection";
 import Button from "../../../components/Button";
 import BackButton from "../../../components/BackButton";
+import RowSelector, { RowOptionType } from "../../../components/RowSelector";
+import { selectPools, selectPrices } from "../../../state/poolsListSlice";
+import { Pool } from "../../../lib";
+import { formatPercent, numberToCompactCurrency } from "../../../lib/numeric";
+import { selectBalances } from "../../../state/userSlice";
+import { selectPositions } from "../../../state/positionsSlice";
+import { ScaledNumber } from "../../../lib/scaled-number";
+import { Position } from "../../../lib/types";
 
 const Container = styled.div`
   position: relative;
@@ -21,7 +30,6 @@ const Header = styled.div`
   font-size: 2.2rem;
   font-weight: 600;
   letter-spacing: 0.25px;
-  margin-bottom: 3rem;
 `;
 
 const ButtonContainer = styled.div`
@@ -34,6 +42,41 @@ const ButtonContainer = styled.div`
 const RegisterTopupPool = () => {
   const { t } = useTranslation();
   const history = useHistory();
+  const pools = useSelector(selectPools);
+  const balances = useSelector(selectBalances);
+  const positions = useSelector(selectPositions);
+  const prices = useSelector(selectPrices);
+  const [pool, setPool] = useState("");
+
+  const options: RowOptionType[] = pools.map((pool: Pool) => {
+    return {
+      value: pool.lpToken.symbol.toLowerCase(),
+      columns: [
+        {
+          label: t("headers.asset"),
+          value: pool.underlying.symbol,
+        },
+        {
+          label: t("headers.apy"),
+          value: formatPercent(pool.apy),
+        },
+        {
+          label: t("headers.tvl"),
+          value: numberToCompactCurrency(pool.totalAssets * prices[pool.underlying.symbol]),
+        },
+        {
+          label: t("headers.deposits"),
+          value: (balances[pool.lpToken.address] || new ScaledNumber())
+            .add(
+              positions
+                .filter((position: Position) => position.depositToken === pool.lpToken.symbol)
+                .reduce((a: ScaledNumber, b: Position) => a.add(b.maxTopUp), new ScaledNumber())
+            )
+            .toCompactUsdValue(prices[pool.underlying.symbol]),
+        },
+      ],
+    };
+  });
 
   return (
     <Container>
@@ -45,6 +88,11 @@ const RegisterTopupPool = () => {
         content={
           <Content>
             <Header>{t("actions.topup.stages.pool.header")}</Header>
+            <RowSelector
+              value={pool}
+              setValue={(value: string) => setPool(value)}
+              options={options}
+            />
             <ButtonContainer>
               <Button
                 primary
