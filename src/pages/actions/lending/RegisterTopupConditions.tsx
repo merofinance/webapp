c54569a4cd375502af4009b801e1630b4a8ba9ea
Chase Manning <me@chasemanning.co.nz>
2021-10-04 22:53:48 +1300

    :rocket: add register topup form and submission

--- src/pages/actions/lending/RegisterTopupConditions.tsx
+++ src/pages/actions/lending/RegisterTopupConditions.tsx
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import { useHistory } from "react-router";
+import { useHistory, useParams } from "react-router";
+import * as yup from "yup";
+import { FormikErrors, useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
 
 import ContentSection from "../../../components/ContentSection";
-import Button from "../../../components/Button";
 import BackButton from "../../../components/BackButton";
+import { AppDispatch } from "../../../app/store";
+import { approve, selectBalance, selectToupAllowance } from "../../../state/userSlice";
+import { useBackd } from "../../../app/hooks/use-backd";
+import { ScaledNumber } from "../../../lib/scaled-number";
+import { INFINITE_APPROVE_AMMOUNT } from "../../../lib/constants";
+import { selectPool } from "../../../state/selectors";
+import { Position } from "../../../lib/types";
+import { hasPendingTransaction } from "../../../state/transactionsSlice";
+import NewPositionConfirmation from "../../pool/NewPositionConfirmation";
+import ApproveThenAction from "../../../components/ApproveThenAction";
+import RegisterTopupInput from "./RegisterTopupInput";
+
+interface TopupParams {
+  address: string;
+  protocol: string;
+  poolName: string;
+}
+
+export interface FormType {
+  threshold: string;
+  singleTopUp: string;
+  maxTopUp: string;
+}
+
+const initialValues: FormType = {
+  threshold: "",
+  singleTopUp: "",
+  maxTopUp: "",
+};
+
+const validationSchema = yup.object().shape({
+  threshold: yup
+    .number()
+    .required("pool.tabs.positions.fields.threshold.required")
+    .moreThan(1.0, "pool.tabs.positions.fields.threshold.greaterThanOne"),
+  singleTopUp: yup
+    .string()
+    .required("pool.tabs.positions.fields.single.required")
+    .test("is-valid-number", "pool.tabs.positions.fields.single.invalid", (s: any) =>
+      ScaledNumber.isValid(s)
+    )
+    .test(
+      "is-positive-number",
+      "pool.tabs.positions.fields.single.positive",
+      (s: any) => !ScaledNumber.fromUnscaled(s).isZero()
+    ),
+  maxTopUp: yup
+    .string()
+    .required("pool.tabs.positions.fields.max.required")
+    .test("is-valid-number", "pool.tabs.positions.fields.max.invalid", (s: any) =>
+      ScaledNumber.isValid(s)
+    )
+    .test(
+      "is-positive-number",
+      "pool.tabs.positions.fields.max.invalid",
+      (s: any) => !ScaledNumber.fromUnscaled(s).isZero()
+    ),
+});
+
+const wrapperFormik = () =>
+  useFormik<FormType>({
+    initialValues,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onSubmit: () => {},
+  });
+export type FormikFormType = ReturnType<typeof wrapperFormik>;
 
 const Container = styled.div`
   position: relative;
@@ -30,6 +98,17 @@ const SubHeader = styled.div`
   opacity: 0.8;
 `;
 
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > div:last-child {
+    justify-content: flex-end;
+  }
+`;
+
 const ButtonContainer = styled.div`
   width: 100%;
   display: flex;
@@ -39,7 +118,77 @@ const ButtonContainer = styled.div`
 
 const RegisterTopupConditions = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch<AppDispatch>();
+  const backd = useBackd();
   const history = useHistory();
+  const { address, protocol, poolName } = useParams<TopupParams>();
+  const pool = useSelector(selectPool(poolName));
+  const balance = useSelector(selectBalance(pool));
+  const loading = useSelector(hasPendingTransaction("Approve"));
+  const [confirming, setConfirming] = useState(false);
+
+  if (!pool) {
+    history.push("/");
+    throw Error("Pool not found");
+  }
+
+  if (!backd) {
+    history.push("/");
+    throw Error("Wallet not connected");
+  }
+
+  const allowance = useSelector(selectToupAllowance(backd, pool));
+
+  const onSubmit = () => {
+    if (approved) setConfirming(true);
+    else executeApprove();
+  };
+
+  const executeApprove = () => {
+    if (!backd) return;
+    dispatch(
+      approve({
+        amount: ScaledNumber.fromUnscaled(INFINITE_APPROVE_AMMOUNT, pool.underlying.decimals),
+        backd,
+        spender: backd.topupActionAddress,
+        token: pool.lpToken,
+      })
+    );
+  };
+
+  const validate = (values: FormType): FormikErrors<FormType> => {
+    const errors: FormikErrors<FormType> = {};
+    const single = ScaledNumber.fromUnscaled(values.singleTopUp, pool.underlying.decimals);
+    const max = ScaledNumber.fromUnscaled(values.maxTopUp, pool.underlying.decimals);
+    if (values.maxTopUp && single.gt(max))
+      errors.singleTopUp = "pool.tabs.positions.fields.single.lessThanMax";
+    if (max.gt(balance)) errors.maxTopUp = "pool.tabs.positions.fields.max.exceedsBalance";
+    return errors;
+  };
+
+  const formik = useFormik({ initialValues, validationSchema, onSubmit, validate });
+
+  const approved = allowance.gte(
+    ScaledNumber.fromUnscaled(formik.values.maxTopUp, pool.underlying.decimals)
+  );
+
+  const position: Position = {
+    protocol,
+    account: address,
+    threshold: ScaledNumber.fromUnscaled(formik.values.threshold),
+    singleTopUp: ScaledNumber.fromUnscaled(formik.values.singleTopUp, pool.underlying.decimals),
+    maxTopUp: ScaledNumber.fromUnscaled(formik.values.maxTopUp, pool.underlying.decimals),
+    maxGasPrice: 0,
+    actionToken: pool.underlying.address,
+    depositToken: pool.lpToken.address,
+  };
+
+  const buttonHoverText = () => {
+    if (!formik.values.threshold) return t("pool.tabs.positions.fields.protocol.hover");
+    if (!formik.values.singleTopUp) return t("pool.tabs.positions.fields.protocol.hover");
+    if (!formik.values.maxTopUp) return t("pool.tabs.positions.fields.protocol.hover");
+    return "";
+  };
 
   return (
     <Container>
@@ -52,17 +201,50 @@ const RegisterTopupConditions = () => {
           <Content>
             <Header>{t("actions.topup.stages.conditions.header")}</Header>
             <SubHeader>{t("actions.topup.stages.conditions.subHeader")}</SubHeader>
-            <ButtonContainer>
-              <Button
-                primary
-                medium
-                width="44%"
-                text={t("components.continue")}
-                click={() => history.push(`/actions/register/skkd`)}
+            <Form noValidate onSubmit={formik.handleSubmit}>
+              <RegisterTopupInput
+                type="number"
+                name="threshold"
+                formik={formik}
+                placeholder="1.4"
               />
-            </ButtonContainer>
+              <RegisterTopupInput
+                type="number"
+                name="singleTopUp"
+                formik={formik}
+                placeholder={`2,000 ${pool.underlying.symbol}`}
+              />
+              <RegisterTopupInput
+                type="number"
+                name="maxTopUp"
+                formik={formik}
+                placeholder={`10,000 ${pool.underlying.symbol}`}
+              />
+              <ButtonContainer>
+                <ApproveThenAction
+                  label={t("pool.tabs.positions.buttons.create")}
+                  action={formik.handleSubmit}
+                  value={ScaledNumber.fromUnscaled(
+                    formik.values.maxTopUp,
+                    pool.underlying.decimals
+                  )}
+                  loading={loading}
+                  disabled={!formik.dirty || !formik.isValid || loading}
+                  token={pool.lpToken}
+                  contract={backd.topupActionAddress}
+                  hoverText={buttonHoverText()}
+                />
+              </ButtonContainer>
+            </Form>
           </Content>
         }
+      />
+      <NewPositionConfirmation
+        show={confirming}
+        close={() => setConfirming(false)}
+        position={position}
+        pool={pool}
+        complete={() => formik.resetForm({ values: initialValues })}
       />
     </Container>
   );
