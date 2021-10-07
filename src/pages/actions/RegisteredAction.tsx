b2e597f3e8187d1a8d7fe430bd981ea509061b20
Chase Manning <me@chasemanning.co.nz>
2021-10-07 23:04:00 +1300

    :iphone: mobile design for registered action

--- src/pages/actions/RegisteredAction.tsx
+++ src/pages/actions/RegisteredAction.tsx
@@ -10,6 +10,7 @@ import { selectPrice } from "../../state/selectors";
 import TopupAction from "./lending/TopupAction";
 
 const StyledRegisteredAction = styled.button`
+  position: relative;
   width: 100%;
   display: flex;
   justify-content: space-between;
@@ -18,34 +19,55 @@ const StyledRegisteredAction = styled.button`
   background-color: rgba(20, 17, 40, 1);
   border-radius: 1.4rem;
   padding: 1.3rem 1.4rem;
-  margin-top: 1rem;
   cursor: pointer;
 
   transition: background-color 0.3s;
   :hover {
     background-color: #1a1438;
   }
+
+  margin-top: 1rem;
+  @media (max-width: 600px) {
+    margin-top: 0.6rem;
+  }
 `;
+
+interface ColumnProps {
+  hideMobile?: boolean;
+}
 
 const Column = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+
+  @media (max-width: 600px) {
+    display: ${(props: ColumnProps) => (props.hideMobile ? "none" : "flex")};
+  }
 `;
 
 const Header = styled.div`
-  font-size: 1.2rem;
   font-weight: 700;
   letter-spacing: 0.2px;
   opacity: 0.6;
+
+  font-size: 1.2rem;
   margin-bottom: 0.5rem;
+  @media (max-width: 600px) {
+    font-size: 1.1rem;
+    margin-bottom: 0.2rem;
+  }
 `;
 
 const Value = styled.div`
-  font-size: 1.8rem;
   font-weight: 700;
   letter-spacing: 0.2px;
+
+  font-size: 1.8rem;
+  @media (max-width: 600px) {
+    font-size: 1.4rem;
+  }
 `;
 
 const ChevronData = styled.td`
@@ -86,11 +108,11 @@ const RegisteredAction = ({ position }: Props) => {
           <Header>{t("actions.registered.columns.locked")}</Header>
           <Value>{position.maxTopUp.toCompactUsdValue(price)}</Value>
         </Column>
-        <Column>
+        <Column hideMobile>
           <Header>{t("actions.suggestions.topup.labels.protocol")}</Header>
           <Value>{position.protocol}</Value>
         </Column>
-        <Column>
+        <Column hideMobile>
           <Header>{t("actions.topup.fields.threshold.label")}</Header>
           <Value>{position.threshold.toCryptoString()}</Value>
         </Column>
