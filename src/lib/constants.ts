bbc8226408eb452e87e67b6f144e08d5985bb79b
Chase Manning <me@chasemanning.co.nz>
2021-10-18 10:17:12 +0100

    :wrench: add constants for gwei and gas

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,11 +2,14 @@ import { BigNumber } from "ethers";
 
 export const ETH_DUMMY_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ETH_DECIMALS = 18;
+export const GWEI_DECIMALS = 9;
 export const INFINITE_APPROVE_AMMOUNT = 10 ** 10;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
+export const GWEI_SCALE = BigNumber.from(10).pow(BigNumber.from(GWEI_DECIMALS));
 export const STAKING_LIVE = false;
 export const INFURA_ID = "f42c3f37e40b4ab1bc5a0d7bf5612d0f";
+export const TOPUP_GAS_COST = BigNumber.from(1_000_000).mul(GWEI_SCALE);
 
 export const chainIds: Record<string, string> = {
   "1": "Mainnet",
