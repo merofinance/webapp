6b438b7ebf7c4cadf66a4737b81b2ce06bc46ace
Chase Manning <me@chasemanning.co.nz>
2021-10-19 15:18:02 +0100

    :rocket: add info icon to live help

--- src/components/LiveHelp.tsx
+++ src/components/LiveHelp.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+
 import AccordionChevron from "./AccordionChevron";
+import gradientInfo from "../assets/ui/gradient-info.svg";
 
 interface LiveHelpProps {
   open?: boolean;
@@ -46,16 +48,14 @@ const StyledLiveHelp = styled.div`
 `;
 
 const Header = styled.button`
-  font-weight: 700;
-  letter-spacing: 0.25px;
-  text-align: left;
   top: 0;
   left: 0;
   position: absolute;
   width: 100%;
   cursor: pointer;
+  display: flex;
+  align-items: center;
 
-  font-size: 2.4rem;
   margin-bottom: 0.6rem;
   height: 5.4rem;
   padding-left: 1.6rem;
@@ -63,8 +63,22 @@ const Header = styled.button`
     margin-bottom: 0;
     height: 4.8rem;
     width: 100%;
-    font-size: 1.8rem;
     background: none;
+  }
+`;
+
+const HeaderIcon = styled.img`
+  width: 2.6rem;
+  margin-right: 1rem;
+`;
+
+const HeaderText = styled.div`
+  font-weight: 700;
+  letter-spacing: 0.25px;
+
+  font-size: 2.4rem;
+  @media (max-width: 1439px) {
+    font-size: 1.8rem;
     line-height: 42px;
   }
 `;
@@ -97,8 +111,15 @@ const Content = styled.div`
   }
 `;
 
+const Suggestion = styled.div`
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 2.1rem;
+  letter-spacing: 0.42px;
+`;
+
 const LiveHelp = (): JSX.Element => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [open, setOpen] = useState(true);
   const isWide = i18n.language === "ja";
 
@@ -107,8 +128,16 @@ const LiveHelp = (): JSX.Element => {
       <ChevronContainer>
         <AccordionChevron open={open} />
       </ChevronContainer>
-      <Header onClick={() => setOpen(!open)}>Meow</Header>
-      <Content>meow</Content>
+      <Header onClick={() => setOpen(!open)}>
+        <HeaderIcon src={gradientInfo} />
+        <HeaderText>{t("liveHelp.header")}</HeaderText>
+      </Header>
+      <Content>
+        <Suggestion>
+          We notice you have entered a threshold top-up of 1.1. This is very close to liquidation
+          and in times of market volatility we suggest at least 1.2 or more.
+        </Suggestion>
+      </Content>
     </StyledLiveHelp>
   );
 };
