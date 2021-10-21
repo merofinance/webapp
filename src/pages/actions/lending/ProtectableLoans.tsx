96c009939c3b71b329a510e1c967720afdc2cb4a
Chase Manning <me@chasemanning.co.nz>
2021-10-21 16:00:21 +0100

    :lipstick: add backd helper design for protectable loans

--- src/pages/actions/lending/ProtectableLoans.tsx
+++ src/pages/actions/lending/ProtectableLoans.tsx
@@ -2,31 +2,40 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+
 import { Loan, Position } from "../../../lib/types";
 import { selectLoans } from "../../../state/lendingSlice";
 import { selectPositions } from "../../../state/positionsSlice";
 import ProtectableLoan from "./ProtectableLoan";
+import logo from "../../../assets/logo/logo.svg";
 
 const StyledProtectableLoans = styled.div`
+  position: relative;
   width: 100%;
   display: flex;
   flex-direction: column;
-  background-color: rgba(255, 255, 255, 0.1);
   border-radius: 1.4rem;
-  padding: 2.4rem 1.6rem;
+  padding: 1.6rem;
+  padding-bottom: 2.4rem;
   margin-bottom: 2.4rem;
+
+  // Background
+  border: 1px solid transparent;
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  background-image: linear-gradient(#252140, #252140),
+    linear-gradient(to right, var(--primary-gradient), var(--secondary-gradient));
 `;
 
 const Header = styled.div`
   font-weight: 700;
   letter-spacing: 0.25px;
+  margin-left: 4rem;
 
   font-size: 2.4rem;
-  line-height: 4.2rem;
-  margin-bottom: 0.2rem;
+  margin-bottom: 1rem;
   @media (max-width: 600px) {
     font-size: 1.8rem;
-    line-height: 2.2rem;
     margin-bottom: 0.3rem;
   }
 `;
@@ -43,6 +52,14 @@ const SubHeader = styled.div`
     font-size: 1.2rem;
     line-height: 1.7rem;
   }
+`;
+
+const BackdHelper = styled.img`
+  left: -1rem;
+  top: -1rem;
+  position: absolute;
+  width: 4.8rem;
+  margin-right: 1rem;
 `;
 
 const ProtectableLoans = () => {
@@ -66,6 +83,7 @@ const ProtectableLoans = () => {
       {protectableLoans.map((loan: Loan) => (
         <ProtectableLoan key={loan.protocol} loan={loan} />
       ))}
+      <BackdHelper src={logo} />
     </StyledProtectableLoans>
   );
 };
