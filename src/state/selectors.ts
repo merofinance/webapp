545c77657227b91ef3bf347dd2c22061e7827110
Chase Manning <me@chasemanning.co.nz>
2021-11-08 18:20:38 +0100

    :bug: pool statistics not showing loading indicator

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -47,75 +47,73 @@ export function selectPoolTotalLocked(
     if (!pool || !prices || !balances || !positions) return undefined;
 
     let total = new ScaledNumber();
-    positions.forEach((position: Position) => {
+    for (let i = 0; i < positions.length; i++) {
       const price = prices[pool.underlying.symbol];
       if (!price) return undefined;
-      total = total.add(position.maxTopUp.mul(price));
-    });
+      total = total.add(positions[i].maxTopUp.mul(price));
+    }
     return total;
   };
 }
 
-export function selectTotalLocked(): Selector<RootState, ScaledNumber | undefined> {
+export function selectTotalLocked(): Selector<RootState, ScaledNumber | null> {
   return (state: RootState) => {
     const pools = useSelector(selectPools);
     const prices = useSelector(selectPrices);
     const balances = useSelector(selectBalances);
     const positions = useSelector(selectPositions);
 
-    if (!pools || !prices || !balances || !positions) return undefined;
+    if (!pools || !prices || !balances || !positions) return null;
 
     let total = new ScaledNumber();
-    positions.forEach((position: Position) => {
+    for (let i = 0; i < positions.length; i++) {
       const pool = pools.filter(
-        (pool: Pool) => pool.underlying.address === position.actionToken
+        (pool: Pool) => pool.underlying.address === positions[i].actionToken
       )[0];
-      if (!pool) return undefined;
+      if (!pool) return null;
       const price = prices[pool.underlying.symbol];
-      if (!price) return undefined;
-      total = total.add(position.maxTopUp.mul(price));
-    });
+      if (!price) return null;
+      total = total.add(positions[i].maxTopUp.mul(price));
+    }
     return total;
   };
 }
 
-export function selectDeposits(
-  pool: Optional<Pool>
-): Selector<RootState, ScaledNumber | undefined> {
+export function selectDeposits(pool: Optional<Pool>): Selector<RootState, ScaledNumber | null> {
   return (state: RootState) => {
-    if (!pool) return undefined;
+    if (!pool) return null;
     const locked = useSelector(selectLocked(pool));
     const balance = useSelector(selectBalance(pool));
-    if (!locked || !balance) return undefined;
+    if (!locked || !balance) return null;
     return locked.add(balance);
   };
 }
 
-export function selectTotalBalance(): Selector<RootState, ScaledNumber | undefined> {
+export function selectTotalBalance(): Selector<RootState, ScaledNumber | null> {
   return (state: RootState) => {
     const pools = useSelector(selectPools);
     const prices = useSelector(selectPrices);
     const balances = useSelector(selectBalances);
     const positions = useSelector(selectPositions);
 
-    if (!pools || !prices || !balances || !positions) return undefined;
+    if (!pools || !prices || !balances || !positions) return null;
 
     let total = new ScaledNumber();
-    pools.forEach((pool: Pool) => {
-      const balance = balances[pool.lpToken.address];
-      const price = prices[pool.underlying.symbol];
-      if (!price || !balance) return undefined;
+    for (let i = 0; i < pools.length; i++) {
+      const balance = balances[pools[i].lpToken.address];
+      const price = prices[pools[i].underlying.symbol];
+      if (!price || !balance) return null;
       total = total.add(balance.mul(price));
-    });
+    }
     return total;
   };
 }
 
-export function selectTotalDeposits(): Selector<RootState, ScaledNumber | undefined> {
+export function selectTotalDeposits(): Selector<RootState, ScaledNumber | null> {
   return (state: RootState) => {
     const totalLocked = useSelector(selectTotalLocked());
     const totalBalance = useSelector(selectTotalBalance());
-    if (!totalLocked || !totalBalance) return undefined;
+    if (!totalLocked || !totalBalance) return null;
     return totalLocked?.add(totalBalance);
   };
 }
