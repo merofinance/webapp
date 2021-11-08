db6ecd42c565319535b98abfa2ebb01e56d36f2e
Chase Manning <me@chasemanning.co.nz>
2021-11-08 16:24:49 +0100

    :sparkles: create locked and deposits selectors

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { Selector } from "reselect";
 import { RootState } from "../app/store";
-import { Optional, Pool } from "../lib/types";
+import { ScaledNumber } from "../lib/scaled-number";
+import { Optional, Pool, Position } from "../lib/types";
+import { selectPoolPositions } from "./positionsSlice";
+import { selectBalance } from "./userSlice";
 
 export function selectPool(poolName: string): (state: RootState) => Optional<Pool> {
   return (state: RootState) =>
@@ -17,4 +21,27 @@ export function getAddress(addressOrPool: string | Optional<Pool>): Optional<str
 
 export function selectPrice(pool: Optional<Pool>): Selector<RootState, number | undefined> {
   return (state: RootState) => (pool ? state.pools.prices[pool.underlying.symbol] : undefined);
+}
+
+export function selectLocked(pool: Optional<Pool>): Selector<RootState, ScaledNumber | undefined> {
+  return (state: RootState) => {
+    if (!pool) return undefined;
+    const positions = useSelector(selectPoolPositions(pool));
+    return positions.reduce(
+      (a: ScaledNumber, b: Position) => a.add(b.maxTopUp),
+      new ScaledNumber()
+    );
+  };
+}
+
+export function selectDeposits(
+  pool: Optional<Pool>
+): Selector<RootState, ScaledNumber | undefined> {
+  return (state: RootState) => {
+    if (!pool) return undefined;
+    const locked = useSelector(selectLocked(pool));
+    const balance = useSelector(selectBalance(pool));
+    if (!locked || !balance) return undefined;
+    return locked.add(balance);
+  };
 }
