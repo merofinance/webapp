365819800b901b0673c425697f2b12f5f1d5d8e7
Chase Manning <me@chasemanning.co.nz>
2021-12-09 16:59:36 +0000

    :fire: remove post test actions from action page spec

--- cypress/integration/action-page_spec.js
+++ cypress/integration/action-page_spec.js
@@ -607,37 +607,3 @@ describe("Existing Topup View", () => {
     cy.get("#register-action-button").contains("Register an Action");
   });
 });
-
-describe("Post test actions", () => {
-  it("Should Innitialise Web3", () => {
-    initWeb3("/pools");
-    cy.get('[id="walletConnect.wallets.metaMask"]').click();
-    cy.get("#pool-row-bdai", { timeout: WEB3_TIMEOUT }).click();
-  });
-  it("Should withdraw DAI", () => {
-    cy.get('[id="pool.tabs.withdraw.tab"]').click();
-    cy.wait(30_000);
-    cy.get("#available-amount", { timeout: WEB3_TIMEOUT }).contains(".", {
-      timeout: WEB3_TIMEOUT,
-    });
-    cy.get("#input-button").click();
-    cy.get("#withdraw-button").click();
-    cy.get("#withdrawal-confirmation-popup-button").click();
-    cy.get("#withdrawal-confirmation-popup-button").should("be.disabled");
-    cy.get("#withdrawal-confirmation-popup-exit").click();
-    cy.get("#withdrawal-confirmation-popup-header").should("not.be.visible");
-    cy.get("#desktop-connector").click();
-    cy.get("#account-details-transactions div", { timeout: WEB3_TIMEOUT })
-      .first()
-      .contains("Withdraw");
-    cy.get("#connector-loading-indicator", { timeout: WEB3_TIMEOUT }).should(
-      "have.css",
-      "opacity",
-      "0"
-    );
-    cy.get("#connection-details-popup-exit").click();
-  });
-  it("Should return DAI", () => {
-    returnDai();
-  });
-});
