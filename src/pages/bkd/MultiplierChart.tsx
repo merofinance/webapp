6f053d5a7a95e6035a69493367db282f1161b6ea
Chase Manning <me@chasemanning.co.nz>
2022-01-12 11:44:54 +0000

    :art: change multiplier chart to use line chart

--- src/pages/bkd/MultiplierChart.tsx
+++ src/pages/bkd/MultiplierChart.tsx
@@ -1,29 +1,6 @@
-import { useLayoutEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import dateFormat from "dateformat";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
-} from "chart.js";
-import { Chart } from "react-chartjs-2";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler
-);
+import LineChart from "../../components/LineChart";
 
 const StyledMultiplierChart = styled.div`
   width: 100%;
@@ -33,27 +10,6 @@ const StyledMultiplierChart = styled.div`
   background: rgba(21, 14, 59, 0.5);
   box-shadow: 0px 0px 12px rgba(23, 18, 22, 0.05);
   margin-bottom: 2.4rem;
-`;
-
-const ChartContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 21.7rem;
-  background-color: rgba(13, 8, 42, 1);
-  border-radius: 14px;
-  padding-left: 1rem;
-`;
-
-interface IndicatorProps {
-  percent: number;
-}
-
-const ProgressIndicator = styled.div`
-  position: absolute;
-  height: calc(100% - 2.4rem);
-  border-right: solid 1px white;
-  left: ${(props: IndicatorProps) => `${Math.round(props.percent * 100)}%`};
-  top: 0;
 `;
 
 const EndDate = styled.div`
@@ -68,128 +24,23 @@ const EndDate = styled.div`
 `;
 
 const MultiplierChart = (): JSX.Element => {
-  const chart = useRef<ChartJS>(null);
-  const [gradient, setGradient] = useState<CanvasGradient>();
-  const [fill, setFill] = useState<CanvasGradient>();
-  const stkBkd = 430;
-
-  const startDate = new Date();
-  startDate.setDate(new Date().getDate() - 100);
-
-  const loadColors = () => {
-    if (!chart.current) {
-      setTimeout(() => loadColors(), 50);
-      return;
-    }
-    const { height } = chart.current.canvas.getBoundingClientRect();
-    const { width } = chart.current.canvas.getBoundingClientRect();
-
-    const gradient_ = chart.current.ctx.createLinearGradient(0, 0, 0, height);
-    gradient_.addColorStop(1, "#C532F9");
-    gradient_.addColorStop(0, "#32B2E5");
-    setGradient(gradient_);
-
-    const offset = 16;
-    const fill_ = chart.current.ctx.createLinearGradient(
-      width / 2 - offset,
-      0,
-      width / 2 + offset,
-      height
-    );
-    fill_.addColorStop(0, "rgba(50, 178, 229, 0.15)");
-    fill_.addColorStop(1, "rgba(50, 178, 229, 0)");
-    // fill_.addColorStop(0, "#C532F9");
-    // fill_.addColorStop(0.48, "#C532F9");
-    // fill_.addColorStop(0.49, "green");
-    // fill_.addColorStop(0.5, "green");
-    // fill_.addColorStop(0.51, "#32B2E5");
-    // fill_.addColorStop(1, "#32B2E5");
-    setFill(fill_);
-  };
-
-  useLayoutEffect(() => {
-    loadColors();
-  }, []);
-
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: false,
-      },
-    },
-    elements: {
-      point: {
-        radius: 10,
-        borderWidth: 0,
-        hoverBorderWidth: 0,
-        borderColor: "transparent",
-        backgroundColor: "transparent",
-      },
-    },
-    scales: {
-      y: {
-        beginAtZero: false,
-        grid: {
-          display: true,
-          color: "rgba(22, 42, 85, 1)",
-        },
-        ticks: {
-          font: {
-            size: 12,
-          },
-
-          stepSize: 1,
-          callback: (value: any) => {
-            return `${value}x`;
-          },
-        },
-      },
-      x: {
-        display: false,
-      },
-    },
-  };
-
-  const data = {
-    labels: Array.from(Array(365).keys()).map((key: number) => {
-      const today = new Date();
-      today.setDate(today.getDate() - 100 + key);
-      return dateFormat(today, "yyyy-m-d");
-    }),
-    color: "red",
-    datasets: [
-      {
-        label: "stkBKD balance",
-        data: Array.from(Array(365).keys()).map((key: number) => {
-          const max = 5;
-          return ((max - 1) / 365) * key + 1;
-        }),
-        backgroundColor: gradient,
-        radius: 3,
-        borderColor: "transparent",
-        borderWidth: 20,
-        fill: {
-          target: "origin",
-          above: fill,
-        },
-      },
-    ],
-  };
-
   const endDate = new Date();
   endDate.setDate(endDate.getDate() - 100 + 365);
 
+  const data = Array.from(Array(365).keys()).map((key: number) => {
+    const max = 5;
+    return ((max - 1) / 365) * key + 1;
+  });
+
+  const labels = Array.from(Array(365).keys()).map((key: number) => {
+    const today = new Date();
+    today.setDate(today.getDate() - 100 + key);
+    return dateFormat(today, "yyyy-m-d");
+  });
+
   return (
     <StyledMultiplierChart>
-      <ChartContainer>
-        <Chart ref={chart} type="line" data={data} options={options} />
-        <ProgressIndicator percent={stkBkd / 1000} />
-      </ChartContainer>
+      <LineChart chartData={data} chartLabels={labels} />
       <EndDate>{dateFormat(endDate, "yyyy-m-d")}</EndDate>
     </StyledMultiplierChart>
   );
