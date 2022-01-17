e75d4685e4653e8c3676d1c9227db8cce393a39d
Chase Manning <me@chasemanning.co.nz>
2022-01-17 09:45:20 +0200

    :art: change to useing basic card

--- src/components/SummaryStatistics.tsx
+++ src/components/SummaryStatistics.tsx
@@ -1,15 +1,5 @@
-import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import Button from "./Button";
-
-export interface SummaryStatisticType {
-  label: string;
-  value: string;
-  subValue?: string;
-  buttonText?: string;
-  buttonAction?: () => void;
-  primary?: boolean;
-}
+import BasicCard, { BasicCardType } from "./BasicCard";
 
 const StyledSummaryStatistics = styled.div`
   display: flex;
@@ -26,107 +16,23 @@ const StyledSummaryStatistics = styled.div`
   }
 `;
 
-interface ContainerType {
-  primary?: boolean;
-}
-
-const Container = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #16122e;
-  border-radius: 14px;
-  margin: 0 0.8rem;
-
-  border: ${(props: ContainerType) => (props.primary ? "1px" : "0px")} solid transparent;
-  background: linear-gradient(#16122e, #16122e),
-    ${(props: ContainerType) =>
-      props.primary
-        ? "linear-gradient(to right, var(--primary), var(--secondary))"
-        : "linear-gradient(#16122e, #16122e)"};
-  background-origin: border-box;
-  background-clip: padding-box, border-box;
-
-  max-width: 40.7rem;
-  padding: 3.3rem 0;
-  @media (max-width: 600px) {
-    width: 100%;
-    max-width: 100%;
-    padding: 2rem 0;
-    margin: 0;
-  }
-`;
-
-const Label = styled.div`
-  font-weight: 700;
-  font-size: 1.4rem;
-  line-height: 2rem;
-  letter-spacing: 0.15px;
-  color: var(--sub);
-  text-transform: capitalize;
-
-  @media (max-width: 600px) {
-  }
-`;
-
-const Number = styled.div`
-  font-weight: 700;
-  line-height: 4.2rem;
-  letter-spacing: 0.25px;
-
-  font-size: 2.8rem;
-  @media (max-width: 600px) {
-    font-size: 2.4rem;
-  }
-`;
-
-const SubValue = styled.div`
-  font-weight: 500;
-  letter-spacing: 0.14px;
-  opacity: 0.5;
-
-  font-size: 1.4rem;
-  @media (max-width: 600px) {
-    font-size: 1.3rem;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  height: 1rem;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transform: translateY(3.5rem);
-`;
-
 interface Props {
-  statistics: SummaryStatisticType[];
+  statistics: BasicCardType[];
 }
 
 const SummaryStatistics = ({ statistics }: Props): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <StyledSummaryStatistics>
-      {statistics.map((statistic: SummaryStatisticType) => (
-        <Container key={statistic.label} primary={statistic.primary}>
-          <Label>{t(statistic.label)}</Label>
-          <Number>{statistic.value}</Number>
-          {statistic.subValue && <SubValue>{t(statistic.subValue)}</SubValue>}
-          {statistic.buttonText && statistic.buttonAction && (
-            <ButtonContainer>
-              <Button
-                primary
-                medium
-                width="13rem"
-                text={statistic.buttonText}
-                click={statistic.buttonAction}
-              />
-            </ButtonContainer>
-          )}
-        </Container>
+      {statistics.map((statistic: BasicCardType) => (
+        <BasicCard
+          key={statistic.label}
+          label={statistic.label}
+          value={statistic.value}
+          subValue={statistic.subValue}
+          buttonText={statistic.buttonText}
+          buttonAction={statistic.buttonAction}
+          primary={statistic.primary}
+        />
       ))}
     </StyledSummaryStatistics>
   );
