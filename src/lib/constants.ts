54c7f3b7cd4826e09a330bad7f3daccd495b4396
Chase Manning <me@chasemanning.co.nz>
2022-01-26 13:49:04 +0000

    :wrench: add shielded feature toggle

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,10 @@
 import { BigNumber } from "ethers";
 
+// Feature toggles
+export const STAKING_LIVE = false;
+export const SHIELDED = false;
+
+// Variables
 export const ETH_DUMMY_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ETH_DECIMALS = 18;
 export const GWEI_DECIMALS = 9;
@@ -7,13 +12,11 @@ export const INFINITE_APPROVE_AMMOUNT = 10 ** 10;
 export const DEFAULT_DECIMALS = ETH_DECIMALS;
 export const DEFAULT_SCALE = BigNumber.from(10).pow(DEFAULT_DECIMALS);
 export const GWEI_SCALE = BigNumber.from(10).pow(BigNumber.from(GWEI_DECIMALS));
-export const STAKING_LIVE = false;
 export const INFURA_ID = "f42c3f37e40b4ab1bc5a0d7bf5612d0f";
 export const TOPUP_GAS_COST = BigNumber.from(1_000_000).mul(GWEI_SCALE);
 export const RECOMMENDED_THRESHOLD = 1.2;
 export const MILLISECONDS_PER_YEAR = 365 * 24 * 60 * 60 * 1000;
 export const DATE_FORMAT = "mmm-d-yyyy";
-
 export const TOPUP_ACTION_ROUTE = "/actions/register/topup";
 
 export const chainIds: Record<string, string> = {
