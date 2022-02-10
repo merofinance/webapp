8e1e48a32dd4681cc105ee932245cf5c01f53c21
Chase Manning <me@chasemanning.co.nz>
2022-02-10 15:45:15 +0000

    :fire: remove switch to testnet button

--- src/pages/landing/CallToActionButton.tsx
+++ src/pages/landing/CallToActionButton.tsx
@@ -1,10 +1,6 @@
-import { useWeb3React } from "@web3-react/core";
-
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import { useIsLive } from "../../app/hooks/use-is-live";
 import Button from "../../components/Button";
-import { changeNetwork } from "../../lib/web3";
 
 interface Props {
   hero?: boolean;
@@ -12,21 +8,11 @@ interface Props {
 
 const CallToActionButton = ({ hero }: Props): JSX.Element => {
   const { t } = useTranslation();
-  const { protocolLive } = useIsLive();
-  const { active } = useWeb3React();
   const navigate = useNavigate();
 
   return (
-    <Button
-      primary
-      hero={hero}
-      large
-      click={() => {
-        if (!active || protocolLive) navigate("/pools");
-        else changeNetwork(42);
-      }}
-    >
-      {!active || protocolLive ? t("landingPage.viewPools") : t("landingPage.changeNetwork")}
+    <Button primary hero={hero} large click={() => navigate("/pools")}>
+      {t("landingPage.viewPools")}
     </Button>
   );
 };
