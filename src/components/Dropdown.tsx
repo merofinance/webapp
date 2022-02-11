bd4aa5080ae8f825107bdd22fa67117817c4e690
Chase Manning <me@chasemanning.co.nz>
2022-02-11 09:48:40 +0000

    :rocket: add options to dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -44,6 +44,45 @@ const Arrow = styled.img`
   transition: 0.3s transform;
 `;
 
+const ExitEvent = styled.button`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 200vw;
+  height: 200vh;
+  transform: translate(-50%, -50%);
+`;
+
+const Options = styled.div`
+  position: absolute;
+  top: calc(100% + 1rem);
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 1.7rem 0;
+  background: #423b68;
+  box-shadow: 0px 0px 12px 0px #00000066;
+  border: 1px solid #554d82;
+  border-radius: 14px;
+`;
+
+const Option = styled.button`
+  padding: 0 1.4rem;
+  height: 3.6rem;
+  font-size: 1.8rem;
+  font-weight: 500;
+  letter-spacing: 0.15px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  text-transform: capitalize;
+
+  background: #423b68;
+  transition: all 0.3s;
+  :hover {
+    background: #322b58;
+  }
+`;
+
 interface Props {
   label: string;
   options: DropdownOptionType[];
@@ -60,6 +99,24 @@ const Dropdown = ({ label, options }: Props): JSX.Element => {
           <Arrow open={open} src={arrow} alt="Dropdown chevron" />
         </ArrowContainer>
       </DropdownButton>
+      {open && (
+        <>
+          <ExitEvent onClick={() => setOpen(false)} />
+          <Options>
+            {options.map((option: DropdownOptionType) => (
+              <Option
+                key={option.label}
+                onClick={() => {
+                  option.action();
+                  setOpen(false);
+                }}
+              >
+                {option.label}
+              </Option>
+            ))}
+          </Options>
+        </>
+      )}
     </StyledDropdown>
   );
 };
