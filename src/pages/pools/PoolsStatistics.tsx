c79db4cb29a1ddc42d568af4aa36ae077dea7683
Chase Manning <me@chasemanning.co.nz>
2022-02-18 10:25:22 +0000

    :rocket: hide actions total on mainnet

--- src/pages/pools/PoolsStatistics.tsx
+++ src/pages/pools/PoolsStatistics.tsx
@@ -1,43 +1,45 @@
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
+import { useWeb3React } from "@web3-react/core";
 
-import Statistics from "../../components/Statistics";
+import Statistics, { StatisticType } from "../../components/Statistics";
 import { formatCurrency } from "../../lib/numeric";
 import {
   selectUsersTotalUsdEverywhere,
   selectUsersTotalUsdLocked,
 } from "../../state/valueSelectors";
+import { ACTIONS_LIVE } from "../../lib/constants";
 
 const PoolsStatistics = (): JSX.Element => {
   const { t } = useTranslation();
+  const { chainId } = useWeb3React();
   const usersTotalUsdLocked = useSelector(selectUsersTotalUsdLocked);
   const usersTotalUsdEverywhere = useSelector(selectUsersTotalUsdEverywhere);
 
-  return (
-    <Statistics
-      statistics={[
-        {
-          header: t("pools.statistics.deposits.header"),
-          tooltip: t("pools.statistics.deposits.tooltip"),
-          value: usersTotalUsdEverywhere
-            ? formatCurrency(Number(usersTotalUsdEverywhere.toString()))
-            : null,
-        },
-        {
-          header: t("pools.statistics.locked.header"),
-          tooltip: t("pools.statistics.locked.tooltip"),
-          value: usersTotalUsdLocked
-            ? formatCurrency(Number(usersTotalUsdLocked.toString()))
-            : null,
-        },
-        // {
-        //   header: t("pools.statistics.rewards.header"),
-        //   tooltip: t("pools.statistics.rewards.tooltip"),
-        //   value: "$0.00",
-        // },
-      ]}
-    />
-  );
+  const statistics: StatisticType[] = [
+    {
+      header: t("pools.statistics.deposits.header"),
+      tooltip: t("pools.statistics.deposits.tooltip"),
+      value: usersTotalUsdEverywhere
+        ? formatCurrency(Number(usersTotalUsdEverywhere.toString()))
+        : null,
+    },
+  ];
+
+  if (ACTIONS_LIVE || chainId !== 1)
+    statistics.push({
+      header: t("pools.statistics.locked.header"),
+      tooltip: t("pools.statistics.locked.tooltip"),
+      value: usersTotalUsdLocked ? formatCurrency(Number(usersTotalUsdLocked.toString())) : null,
+    });
+
+  // statistics.push({
+  //   header: t("pools.statistics.rewards.header"),
+  //   tooltip: t("pools.statistics.rewards.tooltip"),
+  //   value: "$0.00",
+  // });
+
+  return <Statistics statistics={statistics} />;
 };
 
 export default PoolsStatistics;
