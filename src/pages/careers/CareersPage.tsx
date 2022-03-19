0142f0dd8dc04bd38b8318a147eb30814ed67ae0
Chase Manning <me@chasemanning.co.nz>
2022-03-19 10:15:38 +0000

    :sparkles: create careers page component

---
+++ src/pages/careers/CareersPage.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { useDevice } from "../../app/hooks/use-device";
+import Radio from "../../components/Radio";
+import { Header1, Header5, Paragraph } from "../../styles/Headers";
+import Career from "./Career";
+import { careers, CareerType, departments } from "./careers";
+
+const StyledCareersPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 770px;
+`;
+
+const Header = styled(Header5)`
+  width: 100%;
+  text-align: left;
+  margin-top: 8rem;
+
+  @media (max-width: 600px) {
+    margin-top: 4rem;
+  }
+`;
+
+const Positions = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+`;
+
+const HeaderRow = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1.7rem;
+  margin-top: 2rem;
+
+  @media (max-width: 600px) {
+    div:nth-child(2) {
+      display: none;
+    }
+  }
+`;
+
+const PositionHeader = styled.div`
+  flex: 1;
+  text-align: left;
+  font-weight: 700;
+  line-height: 2.4rem;
+  letter-spacing: 0.15px;
+  opacity: 0.6;
+
+  font-size: 1.4rem;
+  @media (max-width: 600px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const ChevronHeader = styled.div`
+  width: 2.4rem;
+`;
+
+const CareersPage = (): JSX.Element => {
+  const [filter, setFilter] = useState("");
+  const { isMobile } = useDevice();
+
+  return (
+    <StyledCareersPage>
+      <Header1>Backd Careers</Header1>
+      <Paragraph>
+        Backd is a yield aggregation and liquidity delegation protocol on Ethereum. Backd relies on
+        different yield-generating strategies, which allocate liquidity to various protocols in
+        order to generate competitive returns. Additionally, liquidity providers are able to specify
+        a set of market conditions on which part of their liquidity will get automatically delegated
+        or used for a specific purpose (for example, to top up an over-collateralized loan).
+      </Paragraph>
+      <Paragraph>
+        While Decentralized Finance (DeFi) offers a lot of potential for building capital efficient
+        products, often times capital is not used efficiently. Furthermore, automation of workflows
+        is still rather limited in DeFi. Backd aims to solve that. By offering a suite of actions,
+        which empower users to automate how their funds should be used, Backd enables users to earn
+        yield on their capital until funds are needed elsewhere.
+      </Paragraph>
+      <Header>Why Backd?</Header>
+      <Paragraph>
+        We are building an extroadinary team. We are also very friendly and easy to get along with.
+        We love to build cool products, ship fast and stay with the newest trends in DeFi. If you
+        are passionate about DeFi, enjoy working with others and solving challenging engineering
+        problems then this role could be a great fit.
+      </Paragraph>
+      <Header>Open positions at Backd</Header>
+      <Positions>
+        <Radio
+          hideDisabled={isMobile}
+          options={[
+            {
+              label: "All departments",
+              value: "",
+            },
+            ...departments.map((d) => {
+              return {
+                label: d,
+                value: d,
+                disabledText: careers.some(({ department }) => department === d)
+                  ? ""
+                  : "No roles available",
+              };
+            }),
+          ]}
+          active={filter}
+          setOption={(value: string) => setFilter(value)}
+        />
+        <HeaderRow>
+          <PositionHeader>Role</PositionHeader>
+          <PositionHeader>Department</PositionHeader>
+          <PositionHeader>Type</PositionHeader>
+          <PositionHeader>Location</PositionHeader>
+          <ChevronHeader />
+        </HeaderRow>
+        {careers
+          .filter((career: CareerType) => (!filter ? true : career.department === filter))
+          .map((career: CareerType, index: number) => (
+            <Career key={index} career={career} />
+          ))}
+      </Positions>
+    </StyledCareersPage>
+  );
+};
+
+export default CareersPage;
