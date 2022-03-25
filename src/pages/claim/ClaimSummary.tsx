9232ce063fc13e0bca32ea0a48644eba96386ca5
chase-manning <chase@manning.dev>
2022-03-25 12:02:10 +0000

    :rocket: add content to claim summary

--- src/pages/claim/ClaimSummary.tsx
+++ src/pages/claim/ClaimSummary.tsx
@@ -1,19 +1,41 @@
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import SummaryStatistics from "../../components/SummaryStatistics";
+import { BKD_PRICE } from "../../lib/constants";
+import { selectUserWeightedAverageApy } from "../../state/poolsListSlice";
+import { selectTotalLpGaugeEarned } from "../../state/userSlice";
+import { selectUsersTotalUsdEverywhere } from "../../state/valueSelectors";
 
 const ClaimSummary = (): JSX.Element => {
   const { t } = useTranslation();
+  const usersTotalUsdEverywhere = useSelector(selectUsersTotalUsdEverywhere);
+  const totalLpGaugeEarned = useSelector(selectTotalLpGaugeEarned());
+  const weightedAverageApy = useSelector(selectUserWeightedAverageApy());
 
   return (
     <SummaryStatistics
       statistics={[
         {
           label: t("claim.overview.claimable"),
-          value: "$1,243.34",
+          value: totalLpGaugeEarned ? totalLpGaugeEarned.toUsdValue(BKD_PRICE) : null,
+          subValue: t("claim.overview.claimableSubheader"),
+        },
+        {
+          label: t("claim.overview.daily"),
+          value:
+            usersTotalUsdEverywhere && weightedAverageApy
+              ? usersTotalUsdEverywhere.mul(weightedAverageApy).div(365).toUsdValue(1)
+              : null,
+          subValue: t("claim.overview.yearly", {
+            amount:
+              usersTotalUsdEverywhere && weightedAverageApy
+                ? usersTotalUsdEverywhere.mul(weightedAverageApy).toCompactUsdValue(1)
+                : "$---",
+          }),
         },
         {
           label: t("claim.overview.deposits"),
-          value: "$65,530.34",
+          value: usersTotalUsdEverywhere ? usersTotalUsdEverywhere.toUsdValue(1) : null,
         },
       ]}
     />
