e69654c990c7905f6116a4c149ec0a274b0a9521
chase-manning <chase@manning.dev>
2022-04-25 12:56:57 +0100

    :sparkles: add link for telegram announcements

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -3,6 +3,7 @@ export const TWITTER_LINK = "https://twitter.com/backdfund";
 export const DISCORD_LINK = "https://discord.gg/jpGvaFV3Rv";
 export const GITHUB_LINK = "https://github.com/backdfund";
 export const TELEGRAM_LINK = "https://t.me/backdchat";
+export const TELEGRAM_ANNOUNCEMENTS_LINK = "https://t.me/backdfund";
 export const NEWSLETTER_LINK = "https://backd.substack.com/welcome";
 
 // Blogs
