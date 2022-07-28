eeeab01c7146831730a9b9b13aafae0c8c69d44d
chase-manning <chase@manning.dev>
2022-07-28 14:46:49 +0100

    :bug: not unstaking when withdrawing

--- src/pages/pool/WithdrawButton.tsx
+++ src/pages/pool/WithdrawButton.tsx
@@ -6,8 +6,8 @@ import { ScaledNumber } from "scaled-number";
 
 import Button from "../../components/Button";
 import { Pool } from "../../lib";
-import { selectWithdrawalFee, unstake, withdraw } from "../../state/userSlice";
-import { selectUsersPoolLpHeld, selectUsersPoolLpStaked } from "../../state/valueSelectors";
+import { selectWithdrawalFee, withdraw } from "../../state/userSlice";
+import { selectUsersPoolUnderlyingUnlocked } from "../../state/valueSelectors";
 import { useMero } from "../../app/hooks/use-mero";
 import { AppDispatch } from "../../app/store";
 import { hasPendingTransaction } from "../../state/transactionsSlice";
@@ -32,10 +32,9 @@ const WithdrawalButton = ({ value, pool, complete, valid }: Props): JSX.Element 
   const { t } = useTranslation();
   const dispatch: AppDispatch = useDispatch();
   const mero = useMero();
-  const usersPoolLpStaked = useSelector(selectUsersPoolLpStaked(pool));
   const loading = useSelector(hasPendingTransaction("Withdraw"));
-  const usersPoolLpHeld = useSelector(selectUsersPoolLpHeld(pool));
   const withdrawalFee = useSelector(selectWithdrawalFee(pool));
+  const usersPoolUnderlyingUnlocked = useSelector(selectUsersPoolUnderlyingUnlocked(pool));
 
   const [confirming, setConfirming] = useState(false);
 
@@ -49,33 +48,19 @@ const WithdrawalButton = ({ value, pool, complete, valid }: Props): JSX.Element 
     };
   }, [loading]);
 
-  const executeWithdraw = (amount: ScaledNumber) => {
-    if (!mero || loading || !pool || !withdrawalFee) return;
-    dispatch(withdraw({ mero, pool, amount, withdrawalFee }));
-  };
-
-  const executeUnstake = () => {
-    if (!mero || loading || !usersPoolLpStaked || !pool) return;
-    dispatch(unstake({ mero, pool, amount: usersPoolLpStaked }));
-  };
-
   const submit = () => {
-    if (!valid || !usersPoolLpHeld || !pool) return;
+    if (!valid || !usersPoolUnderlyingUnlocked || !pool || !mero || !withdrawalFee) return;
     const lpValue = value.div(pool.exchangeRate);
 
     // If the amount the user is withdrawing is 99.9% of their total, then withdraw their total
     // This is to avoid the situation where a user withdraws all their tokens other than 0.00000001 DAI from a rounding error
-    const isMaxWithdrawal = usersPoolLpHeld
+    const isMaxWithdrawal = usersPoolUnderlyingUnlocked
       .sub(lpValue)
-      .div(usersPoolLpHeld)
+      .div(usersPoolUnderlyingUnlocked)
       .lte(ScaledNumber.fromUnscaled(0.001, lpValue.decimals));
-    if (isMaxWithdrawal) {
-      executeWithdraw(usersPoolLpHeld);
-      return;
-    }
 
-    if (lpValue.lte(usersPoolLpHeld)) executeWithdraw(lpValue);
-    else executeUnstake();
+    const amount = isMaxWithdrawal ? usersPoolUnderlyingUnlocked : lpValue;
+    dispatch(withdraw({ mero, pool, amount, withdrawalFee }));
   };
 
   if (!pool) return <Loader button />;
