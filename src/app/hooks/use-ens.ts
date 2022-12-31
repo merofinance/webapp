afb0c5f0db4f72298bf826487f05c864ee269757
chase-manning <chase@manning.dev>
2022-12-31 14:04:26 +0000

    :bug: fix too many requests issue

--- src/app/hooks/use-ens.ts
+++ src/app/hooks/use-ens.ts
@@ -1,21 +1,19 @@
 import { useWeb3React } from "@web3-react/core";
-import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import { Optional } from "../../lib/types";
 
 const useENS = (): { ensName: Optional<string>; ensAvatar: Optional<string> } => {
-  const { account } = useWeb3React();
+  const { account, library } = useWeb3React();
   const [ensName, setENSName] = useState<Optional<string>>(null);
   const [ensAvatar, setENSAvatar] = useState<Optional<string>>(null);
 
   const resolveENS = async (): Promise<void> => {
     setENSAvatar(null);
     if (!account) return;
-    const provider = ethers.providers.getDefaultProvider();
-    const ensName = await provider.lookupAddress(account);
+    const ensName = await library.provider.lookupAddress(account);
     setENSName(ensName);
     if (!ensName) return;
-    const resolver = await provider.getResolver(ensName);
+    const resolver = await library.provider.getResolver(ensName);
     if (!resolver) return;
     const avatar = await resolver.getAvatar();
     if (!avatar) return;
