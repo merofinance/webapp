33de8c23ec85d138a187c08a6748cbd1b99919d9
chase-manning <chase@manning.dev>
2023-02-28 12:03:47 +0000

    :sparkles: create pool tvl component

---
+++ src/pages/pool/PoolTvl.tsx
@@ -0,0 +1,26 @@
+import dateFormat from "dateformat";
+
+import useHistoricStats from "../../app/hooks/use-historic-stats";
+import InfoChart from "../../components/InfoChart";
+import { Pool } from "../../lib";
+
+interface Props {
+  pool: Pool | null;
+}
+
+const PoolTvl = ({ pool }: Props) => {
+  const stats = useHistoricStats(pool ? pool.underlying.symbol : null);
+
+  if (!stats) return null;
+
+  const cutoffDate = new Date();
+  cutoffDate.setMonth(cutoffDate.getMonth() - 12);
+  const recentStats = stats?.filter((stat) => stat.date > cutoffDate);
+
+  const data = recentStats.map((stat) => stat.tvlUsd);
+  const labels = recentStats.map((stat) => dateFormat(stat.date, "d mmm"));
+
+  return <InfoChart header="Historic TVL" data={data} labels={labels} />;
+};
+
+export default PoolTvl;
