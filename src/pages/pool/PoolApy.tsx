261ac4ed46ba216df0662fcde7c47e4d4192b9ee
chase-manning <chase@manning.dev>
2023-02-28 12:03:40 +0000

    :sparkles: create pool apy component

---
+++ src/pages/pool/PoolApy.tsx
@@ -0,0 +1,26 @@
+import dateFormat from "dateformat";
+
+import useHistoricStats from "../../app/hooks/use-historic-stats";
+import InfoChart from "../../components/InfoChart";
+import { Pool } from "../../lib";
+
+interface Props {
+  pool: Pool | null;
+}
+
+const PoolApy = ({ pool }: Props) => {
+  const stats = useHistoricStats(pool ? pool.underlying.symbol : null);
+
+  if (!stats) return null;
+
+  const cutoffDate = new Date();
+  cutoffDate.setMonth(cutoffDate.getMonth() - 3);
+  const recentStats = stats?.filter((stat) => stat.date > cutoffDate);
+
+  const data = recentStats.map((stat) => stat.apy);
+  const labels = recentStats.map((stat) => dateFormat(stat.date, "d mmm"));
+
+  return <InfoChart header="Historic APY" data={data} labels={labels} />;
+};
+
+export default PoolApy;
